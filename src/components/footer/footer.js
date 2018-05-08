eb01eef956e9bf1c53b83209c4dc793dae33421c
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2018-05-08 13:41:13 +1000

    Header and footer logos added

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -10,10 +10,13 @@ import compStyles from './footer.module.css'
 import Link from 'gatsby-link';
 import React from 'react';
 
+// Images
+import footerLogo from "../../../site/images/logo/hca-data-portal/logo-hca-white.png"
+
 const Footer = () => (
     <div className={compStyles.footer}>
         <div>
-            <div className={compStyles.logo}><i className='material-icons'>chevron_left</i>logo</div>
+            <div className={compStyles.logo}><i className='material-icons'>chevron_left</i><img src={footerLogo}/></div>
             /* Footer links will need to be iterated backwards */
             <div className={compStyles.links}>
                 <Link to="/">Events/Webinar</Link>
