29b674907ca34a22a25a83687bc61909b8b840b9
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2018-06-11 13:34:02 +1000

    Responsive nav WIP

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -10,6 +10,17 @@ import Link from 'gatsby-link';
 import React from 'react';
 import compStyles from './header.module.css';
 
+var classNames = require('classnames');
+
+let getLinkClassName = () => {
+    return classNames({
+        [compStyles.links]: true,
+        [compStyles.showLinks]: false
+    });
+};
+
+var showLinks = false;
+
 // Images
 import headerLogo from "../../../site/images/logo/hca-data-portal/logo-hca.png"
 
@@ -17,14 +28,31 @@ const Header = ({siteTitle}) => (
     <div className={compStyles.navBar}>
         <div className={compStyles.wrapper}>
             <div className={compStyles.logo}><img src={headerLogo}/></div>
-            <div className={compStyles.links}>
-                <a  href="https://explore.dev.data.humancellatlas.org">Explore</a>
-                <Link to="/">Analyze</Link>
-                <Link to="/">Contribute</Link>
-                <Link to="/learn/how-it-works/data-lifecycle">Learn</Link>
-                <Link to="/">Build</Link>
+            <div className={getLinkClassName()}>
+                <a href="https://explore.dev.data.humancellatlas.org">
+                    <span>Explore</span>
+                    <span>Search for data in the HCA</span>
+                </a>
+                <Link to="/">
+                    <span>Analyze</span>
+                    <span>Find a list of Apps</span>
+                </Link>
+                <Link to="/">
+                    <span>Contribute</span>
+                    <span>Submit your data to the HCA</span>
+                </Link>
+                <Link to="/learn/how-it-works/data-lifecycle">
+                    <span>Learn</span>
+                    <span>Find user guides and how-to’s here</span>
+                </Link>
+                <Link to="/">
+                    <span>Build</span>
+                    <span>Find developer guides and API docs</span>
+                </Link>
             </div>
-            {/*<div className={compStyles.menuDropDown}>Alex S.</div>*/}
+            <div className={compStyles.userDropDown}>Alex S.</div>
+            <div className={compStyles.menuDropDown}>Menu</div>
+            {showLinks ? <div className={compStyles.hcaNavOverlay}></div> : null}
         </div>
     </div>
 );
