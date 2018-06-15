81ec905622ab4e734c4105bf1a1d8a13001c3b04
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2018-06-15 23:12:59 +1000

    New tab closes nav

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -15,6 +15,7 @@ const classNames = require('classnames');
 let active;
 let expanded;
 let initialShowNav = false;
+let tab;
 
 const getNavClassName = (docPath, nav) => {
 
@@ -32,15 +33,24 @@ class Nav extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {showNav: initialShowNav, tab: this.props.docPath.split("/")[2]};
+        this.state = {showNav: initialShowNav};
+        this.getTab();
         this.toggleNav = this.toggleNav.bind(this);
-        console.log(this.state.tab);
     }
+
+    getTab = () => {
+
+        if (tab !== this.props.docPath.split("/")[2]) {
+            this.state.showNav = false;
+            tab = this.props.docPath.split("/")[2]
+        }
+    };
 
     toggleNav = () => {
 
-        // TODO when new tab, navigation is closed
-        // TODO window resize event handler
+        // Navigation closes when new tab selected
+        this.getTab();
+
         // Toggle the navigation open/closed
         this.setState({showNav: !this.state.showNav});
 
@@ -55,6 +65,19 @@ class Nav extends React.Component {
     render() {
         return (
             <div className={compStyles.hcaNav}>
+                <ul className={compStyles.hcaSideNav}>
+                    {siteMap.getNav(this.props.docPath).map((p, i) =>
+                            <div key={i}>
+                                <li className={getNavClassName(this.props.docPath, p)} key={i}><Link
+                                    to={p.key}>{p.name}</Link></li>
+                                {p.children && expanded ?
+                                    <ul>
+                                        {p.children.map((c, j) => <li className={getNavClassName(this.props.docPath, c)}
+                                                                      key={j}><Link
+                                            to={c.key}>{c.name}</Link></li>)}
+                                    </ul> : null}
+                            </div>)}
+                </ul>
                 <ul className={compStyles.hcaSideNav}>
                     <li className={compStyles.select} onClick={this.toggleNav}>
                         <span>Please select</span><i className='material-icons'>keyboard_arrow_down</i>
