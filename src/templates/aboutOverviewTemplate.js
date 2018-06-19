f1dc7a33256c7d9add800c31eb7aa5d228ad1621
noopdog <dave@clevercanary.com>
2018-06-19 15:42:39 -0700

    build for package json

--- src/templates/aboutOverviewTemplate.js
+++ src/templates/aboutOverviewTemplate.js
@@ -29,6 +29,7 @@ export default function Template({
     const {markdownRemark} = data; // data.markdownRemark holds our post data
     const {frontmatter, html} = markdownRemark;
     const docPath = frontmatter.path;
+    const linked = frontmatter.linked;
 
     return (
         <div>
@@ -85,33 +86,7 @@ export default function Template({
                     <div className={compStyles.whatWeProvide}>
                         <h4>What we provide</h4>
                         <div className={compStyles.contentFlex}>
-                            <div>
-                                <div className={compStyles.flexImg}/>
-                                <h4>Services</h4>
-                                <p>Services for uploading, analyzing, and accessing data</p>
-                                <Link to="/">Ingest Service</Link>
-                                <Link to="/">Data Store</Link>
-                                <Link to="/">Data Browser</Link>
-                                <Link to="/">Secondary Analysis</Link>
-                            </div>
-                            <div>
-                                <div className={compStyles.flexImg}/>
-                                <h4>Software</h4>
-                                <p>Software for building your own pipelines and analysis platforms</p>
-                                <Link to="/">Fringilla</Link>
-                                <Link to="/">Condimentum</Link>
-                                <Link to="/">Consectetur</Link>
-                                <Link to="/">Pharetra</Link>
-                                <Link to="/">Fermentum</Link>
-                            </div>
-                            <div>
-                                <div className={compStyles.flexImg}/>
-                                <h4>Standards</h4>
-                                <p>Standards for addressing data quality to enable comparisons between datasets from labs around the world</p>
-                                <Link to="/">Metadata</Link>
-                                <Link to="/">Standards</Link>
-                                <Link to="/">Condimentum</Link>
-                            </div>
+                            {linked.map((link, i) => <div key={i} dangerouslySetInnerHTML={{__html: link.childMarkdownRemark.html}}/>)}
                         </div>
                     </div>
                 </div>
@@ -121,13 +96,25 @@ export default function Template({
 }
 
 export const pageQuery = graphql`
-  query AboutOverviewPostByPath($path: String!) {
+  query AboutPostByPath($path: String!) {
     markdownRemark(frontmatter: { path: { eq: $path } }) {
       html
       frontmatter {
         date(formatString: "MMMM DD, YYYY")
         path
         title
+        componentName
+        linked {
+               childMarkdownRemark{
+               html
+                frontmatter{
+                    path
+                    title
+                    subTitle
+                    githubUrl
+                }
+               }
+              }
       }
     }
   }
