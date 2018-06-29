bde9cbcb7d1fc63e5e366b9f6dc1ff5a20f91dbf
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2018-06-29 18:45:03 +1000

    Header navigation tabs on hover added

---
+++ src/components/headerTabNav/headerTabNav.js
@@ -0,0 +1,56 @@
+/*
+ * Human Cell Atlas
+ * https://www.humancellatlas.org/
+ *
+ * HCA Data Portal header tab nav component.
+ */
+
+// Core dependencies
+import compStyles from './headerTabNav.module.css';
+import {navigateTo} from "gatsby-link";
+import React from 'react';
+
+// App dependencies
+const classNames = require('classnames');
+import * as siteMap from '../../siteMap';
+
+
+class HeaderTabNav extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    getHeaderTabClassName = (right) => {
+        {
+            return classNames({
+                [compStyles.headerTabs]: true,
+                [compStyles.rightAlign]: right
+            });
+        }
+    };
+
+    getTabLink = (tab) => {
+
+        if (tab.children[0]) {
+            this.props.clearActiveLink();
+            return tab.children[0].key;
+        }
+        else {
+            this.props.clearActiveLink();
+            return "/";
+        }
+    };
+
+    render() {
+        return (
+            <div className={this.getHeaderTabClassName(this.props.rightAlign)}>
+                {siteMap.getTabs(this.props.section).map((tab, i) => <div key={i}><p onClick={() => navigateTo(this.getTabLink(tab))}
+                                                                          className={compStyles.s}>{tab.name}</p>
+                </div>)}
+            </div>
+        );
+    }
+}
+
+export default HeaderTabNav;
