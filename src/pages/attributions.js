543a295632c96b845c161143cbc1c2e5d84aeeeb
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2018-08-29 18:05:18 +1000

    Attribution logo

--- src/pages/attributions.js
+++ src/pages/attributions.js
@@ -11,18 +11,24 @@ import React from 'react';
 import Section from "../components/section/section";
 import TabNav from "../components/tabNav/tabNav";
 
+// Images
+import favicon from "../../images/favicon/favicon.png";
+
 const Attributions = () => (
-    <div>
-        <Section sectionTitle={"Attributions"}/>
-        <TabNav noTab={true}/>
-        <div className={compStyles.error}>
-            <div className={compStyles.wrapper}>
-                <div className={compStyles.hcaContent}>
-                    <p>The mosaic circle is a trademark of the Human Cell Atlas consortium, registered by Broad Institute and used with permission.</p>
+        <div>
+            <Section sectionTitle={"Attributions"}/>
+            <TabNav noTab={true}/>
+                <div className={compStyles.wrapper}>
+                    <div className={compStyles.hcaContent}>
+                        <div className={compStyles.attributions}>
+                            <img className={compStyles.mosaic} src={favicon}/>
+                            <p className={compStyles.s}>The mosaic circle is a trademark of the Human Cell Atlas consortium, registered by Broad
+                                Institute and used with permission.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-);
+    )
+;
 
 export default Attributions;
