bba87b37b04bd2cc483cb4db33624fb08d747b1a
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2018-09-04 09:07:04 +1000

    Analyze component frontmatter now markdown

--- src/components/analyze/analyze.js
+++ src/components/analyze/analyze.js
@@ -14,7 +14,6 @@ class Analyze extends React.Component {
 
     constructor(props) {
         super(props);
-        console.log(this.props.linked);
     }
 
     render() {
@@ -22,9 +21,7 @@ class Analyze extends React.Component {
             <div className={compStyles.hcaAnalyze}>
                 {this.props.linked.map((link, i) => <div className={compStyles.hcaAnalyzeList} key={i}>
                     <div>
-                        <h3>{link.childMarkdownRemark.frontmatter.title}</h3>
-                        <p>{link.childMarkdownRemark.frontmatter.subTitle}</p>
-                        {/*<p className={compStyles.xs}>Last update to GitHub: 2 days ago</p>*/}
+                        <div dangerouslySetInnerHTML={{__html: link.childMarkdownRemark.html}}/>
                     </div>
                     <a href={link.childMarkdownRemark.frontmatter.githubUrl} target="_blank" className={compStyles.analyze}>View</a>
                 </div>)}
