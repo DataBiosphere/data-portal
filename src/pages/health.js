3e35504588501ecf190243d53bdae31cb250baa1
noopdog <dave@clevercanary.com>
2018-09-05 21:36:18 +1000

    added health page

---
+++ src/pages/health.js
@@ -0,0 +1,21 @@
+/*
+ * Human Cell Atlas
+ * https://www.humancellatlas.org/
+ *
+ * Demo of second page, keeping temporarily as a reference. TODO delete.
+ */
+
+// Core dependencies
+import React from 'react';
+
+// Core dependencies
+import Section from "../components/section/section";
+
+
+const Health = () => (
+    <div>
+        <Section sectionTitle={"Health - OK"}/>
+    </div>
+);
+
+export default Health;
