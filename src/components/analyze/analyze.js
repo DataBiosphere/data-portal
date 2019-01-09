c068eb1bf5c38f33eec1e36e34a088c671736394
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2019-01-09 10:21:21 +1000

    Analyze detail pages

--- src/components/analyze/analyze.js
+++ src/components/analyze/analyze.js
@@ -6,12 +6,12 @@
  */
 
 // Core dependencies
+import Link from 'gatsby-link';
 import React from 'react';
 
 // Styles
-import compStyles from './analyze.module.css'
-import globalStyles from '../../styles/global.module.css';
-let classNames = require('classnames');
+import compStyles from './analyze.module.css';
+import fontStyles from '../../styles/fontsize.module.css';
 
 class Analyze extends React.Component {
 
@@ -19,14 +19,22 @@ class Analyze extends React.Component {
         super(props);
     }
 
+    getMarkdownFileName = (link) => {
+
+        let path = link.childMarkdownRemark.frontmatter.path.split("/");
+        return ("/" + path[1] + "/" + path[2] + "/" + path[4]);
+    };
+
     render() {
         return (
             <div>
-                {this.props.linked.map((link, i) => <div className={compStyles.hcaAnalyzeList} key={i}>
+                {this.props.linked.map((link, i) => <Link to={this.getMarkdownFileName(link)} className={compStyles.hcaAnalyzeList} key={i}>
                     <div>
-                        <div dangerouslySetInnerHTML={{__html: link.childMarkdownRemark.html}}/></div>
-                    <a href={link.childMarkdownRemark.frontmatter.githubUrl} target="_blank" className={classNames(globalStyles.button, globalStyles.outline, globalStyles.primary)}>View</a>
-                </div>)}
+                        <h5>{link.childMarkdownRemark.frontmatter.title}</h5>
+                        {link.childMarkdownRemark.frontmatter.author ? <p className={fontStyles.xxs}>{link.childMarkdownRemark.frontmatter.author}</p> : null}
+                        <p className={compStyles.description}>{link.childMarkdownRemark.frontmatter.description}</p>
+                    </div>
+                </Link>)}
             </div>
         );
     }
