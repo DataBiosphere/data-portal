92893fe3a99f9330cb324f190ce2b4474ba4d58f
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2019-03-05 13:43:35 +1000

    Core and Modules nested into Types metadata

--- src/templates/metadataTemplate.js
+++ src/templates/metadataTemplate.js
@@ -43,8 +43,6 @@ export default function Template({data}) {
         }
     ).node;
 
-    const coreObject = core.coreEntity + "_core";
-
     const types = data.metadata.edges.filter((x) => {
         return x.node.schemaType === "type";
     }).map(n => n.node);
@@ -53,14 +51,33 @@ export default function Template({data}) {
         return x.node.schemaType === "module";
     }).map(n => n.node);
 
-    // Check if x_core exists
-    const isXCore = (properties) => {
-        return properties.some(p => p.name === coreObject);
+    // Core and Modules comprise of objects or arrays for example within Modules there
+    // is an object named "Death" with an array properties like "time_of_death" and "cause_of_death".
+    // The types metadata will merge any modules or core object where it has been referenced.
+    // For example the type "Donor Organism" includes the property "Death conditions", which is an object of type "death".
+    // "Death conditions" will be replaced by the Module property "death".
+    let typesMerged, objectTitle, objectProperties;
+
+    // Check if the core or modules object (or array) exists within the list of type properties
+    const isXObjectOrArray = (properties, objectTitle) => {
+        return properties.findIndex(property => ((getRegexText(property.itemsRef) === objectTitle || getRegexText(property.objectRef) === objectTitle)));
     };
 
-    // Removal of core entity x_core where x is entity name
-    const removeCoreEntity = (properties) => {
-        return properties.filter(property => property.name !== coreObject);
+    // Regex string
+    const getRegexText = (text) => {
+        return text.toLowerCase().replace(/_/g, " ").replace(/\-/g, " ");
+    };
+
+    // Get Module or Core title and corresponding properties
+    const getObjectTitleAndProperties = (schemaType) => {
+
+        objectTitle = getRegexText(schemaType.title);
+        objectProperties = schemaType.properties;
+
+        return {
+            objectTitle,
+            objectProperties
+        };
     };
 
     // Removal of duplicate properties
@@ -68,39 +85,42 @@ export default function Template({data}) {
         return properties.filter((p, i, a) => a.map(a => a.name).indexOf(p.name) === i);
     };
 
-    // Inserting core into type, removing x_core where x = entity name
-    const typesWithCore = types.map((type) => {
+    // Merge Modules with Core metadata
+    const mergedSchemaCoreWithModules = modules.concat(core);
 
-        if (isXCore(type.properties)) {
-            // Insert core entity x_core, if exists
-            core.properties.map((c, i) => {
-                type.properties.splice(i, 0, c);
-            });
-            // Remove core entity e.g. biomaterial_core
-            type.properties = removeCoreEntity(type.properties);
-            // Remove duplicates e.g. describedBy, schema_version
+    // For each Module and Core, replace any found usages of the module/core with its corresponding properties
+    mergedSchemaCoreWithModules.forEach((coreOrModule => {
+
+        // Get the title for each module/core
+        getObjectTitleAndProperties(coreOrModule);
+        let objectArrayPosition = -1;
+
+        // Replace any found usages of the module/core with its corresponding properties
+        typesMerged = types.filter((type) => {
+
+            // Check to see if there is a type property that is actually a module object or array.
+            // For example, type property of name "Death conditions" is an object of type "death".
+            objectArrayPosition = isXObjectOrArray(type.properties, objectTitle);
+
+            // If a type property array/object exists, insert its equivalent module/core properties.
+            // For example type property of name "Death conditions" would be replaced by the module property "Death"
+            // which comprises of an array of properties like "cause_of_death" and "time_of_death".
+            if (objectArrayPosition >= 0) {
+                objectProperties.map((property, i) => (type.properties.splice((objectArrayPosition + i + 1), 0, property)));
+            }
+
+            // Exclude type property that references its module (object or array).
+            // For example, exclude the type property of name "Death conditions", where it is an object "death" - we have now replaced
+            // this object with its corresponding array (see above).
+            type.properties = type.properties.filter((p, i) => (i !== objectArrayPosition));
+
+            // Remove any duplicate properties e.g. "describeBy"
             type.properties = removeDuplicateProperties(type.properties);
-        }
 
-        return type;
-    });
-
-    // Inserting core into modules, removing x_core where x = entity name
-    const modulesWithCore = modules.map((module) => {
-
-        if (isXCore(module.properties)) {
-            // Insert core entity x_core, if exists
-            core.properties.map((c, i) => {
-                module.properties.splice(i, 0, c);
-            });
-            // Remove core entity e.g. biomaterial_core
-            module.properties = removeCoreEntity(module.properties);
-            // Remove duplicates e.g. describedBy, schema_version
-            module.properties = removeDuplicateProperties(module.properties);
-        }
-
-        return module;
-    });
+            objectArrayPosition = -1;
+            return type;
+        });
+    }));
 
     return (
         <div>
@@ -115,12 +135,7 @@ export default function Template({data}) {
                             dangerouslySetInnerHTML={{__html: html}}
                         />
                         <p className={classNames(fontStyles.xxs, compStyles.xxs)}>* Indicates a required field</p>
-                        <h2>{title} Types</h2>
-                        {typesWithCore.length ? types.map((e, i) => <Metadata entity={e} key={i}/>) :
-                            <div className={fontStyles.s}>No Modules</div>}
-                        <h2>{title} Modules</h2>
-                        {modulesWithCore.length ? modules.map((e, i) => <Metadata entity={e} key={i}/>) :
-                            <div className={fontStyles.s}>No Modules</div>}
+                        {types.map((type, i) => <Metadata entity={type} key={i}/>)}
                     </div>
                 </div>
             </div>
