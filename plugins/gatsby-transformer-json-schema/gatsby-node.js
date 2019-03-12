ec573365586af81fbcb194199b754563f7f8bd5f
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2019-03-12 16:45:11 +1000

    Nested metadata

--- plugins/gatsby-transformer-json-schema/gatsby-node.js
+++ plugins/gatsby-transformer-json-schema/gatsby-node.js
@@ -4,89 +4,94 @@ const path = require(`path`);
 const {createFilePath} = require(`gatsby-source-filesystem`);
 
 
-async function onCreateNode({ node, getNode, actions, loadNodeContent }) {
+async function onCreateNode({node, getNode, actions, loadNodeContent}) {
 
 
-    const { createNode, createParentChildLink } = actions;
+	const {createNode, createParentChildLink} = actions;
 
-    function transformObject(obj, id, type) {
-        const objStr = JSON.stringify(obj)
-        const contentDigest = crypto
-            .createHash(`md5`)
-            .update(objStr)
-            .digest(`hex`)
-        const jsonNode = {
-            ...obj,
-            id,
-            children: [],
-            parent: node.id,
-            internal: {
-                contentDigest,
-                type,
-            },
-        }
-        createNode(jsonNode)
-        createParentChildLink({ parent: node, child: jsonNode })
-    }
-
-
-
-    // We only care about JSON content.
-    if (node.internal.mediaType !== `application/json`) {
-        return
-    }
+	function transformObject(obj, id, type) {
+		const objStr = JSON.stringify(obj)
+		const contentDigest = crypto
+			.createHash(`md5`)
+			.update(objStr)
+			.digest(`hex`)
+		const jsonNode = {
+			...obj,
+			id,
+			children: [],
+			parent: node.id,
+			internal: {
+				contentDigest,
+				type,
+			},
+		}
+		createNode(jsonNode)
+		createParentChildLink({parent: node, child: jsonNode})
+	}
 
 
-    const content = await loadNodeContent(node);
-    const parsedContent = JSON.parse(content);
+	// We only care about JSON content.
+	if (node.internal.mediaType !== `application/json`) {
+		return
+	}
 
 
-    const relativeFilePath = createFilePath({
-        node,
-        getNode,
-        basePath: ""
-    });
+	const content = await loadNodeContent(node);
+	const parsedContent = JSON.parse(content);
 
-    function splitRef(ref) {
-        let regex = /_/g;
-        let splitRef = ref.split('/');
 
-        return splitRef[splitRef.length - 1].split(".")[0].replace(regex, ' ');
-    }
+	const relativeFilePath = createFilePath({
+		node,
+		getNode,
+		basePath: ''
+	});
 
-    const sections = relativeFilePath.split('/');
+	function removeFileType(ref) {
+		return ref.split('.json')[0];
+	}
 
-    const propertyNames = _.keys(parsedContent.properties);
-    const requiredProperties = parsedContent.required || [];
+	function splitRef(ref) {
+		let regex = /_/g;
+		let splitRef = ref.split('/');
 
-    const properties = propertyNames.map((name) =>{
-        return {
-            name: name,
-            description: parsedContent.properties[name].description,
-            itemsRef: parsedContent.properties[name].items ? parsedContent.properties[name].items.$ref ? splitRef(parsedContent.properties[name].items.$ref) : "" : "",
-            itemsType: parsedContent.properties[name].items ? parsedContent.properties[name].items.type : "",
-            objectRef: parsedContent.properties[name].$ref ? splitRef(parsedContent.properties[name].$ref) : "",
-            required: requiredProperties.includes(name),
-            type: parsedContent.properties[name].type,
-            userFriendly: parsedContent.properties[name].user_friendly
-        }
-    });
-    
+		return splitRef[splitRef.length - 1].split('.')[0].replace(regex, ' ');
+	}
 
-    const entity = {
-        title: parsedContent.title,
-        description:parsedContent.description,
-        properties: properties,
-        relativeFilePath: relativeFilePath,
-        schemaType: sections[1], // core, type or module
-        coreEntity: sections[2], // core type biomaterial, project,
-        category: sections[4] ? sections[3] : ""
-    };
+	const sections = relativeFilePath.split('/');
 
-    transformObject(
-        entity,
-        `${node.id}  >>> JSON`,
-        "MetadataSchemaEntity");
+	const propertyNames = _.keys(parsedContent.properties);
+	const requiredProperties = parsedContent.required || [];
+
+	const properties = propertyNames.map((name) => {
+		return {
+			name: name,
+			description: parsedContent.properties[name].description,
+			arrayModuleRef: parsedContent.properties[name].items ? parsedContent.properties[name].items.$ref ? removeFileType(parsedContent.properties[name].items.$ref) : '' : '',
+			arrayName: parsedContent.properties[name].items ? parsedContent.properties[name].items.$ref ? splitRef(parsedContent.properties[name].items.$ref) : '' : '',
+			arrayType: parsedContent.properties[name].items ? parsedContent.properties[name].items.type : '',
+			objectModuleRef: parsedContent.properties[name].$ref ? removeFileType(parsedContent.properties[name].$ref) : '',
+			objectName: parsedContent.properties[name].$ref ? splitRef(parsedContent.properties[name].$ref) : '',
+			required: requiredProperties.includes(name),
+			type: parsedContent.properties[name].type,
+			userFriendly: parsedContent.properties[name].user_friendly
+		}
+	});
+
+
+	const entity = {
+		title: parsedContent.title,
+		description: parsedContent.description,
+		properties: properties,
+		relativeFilePath: relativeFilePath,
+		schemaType: sections[1], // core, type or module
+		coreEntity: sections[2], // core type biomaterial, project,
+		category: sections[4] ? sections[3] : ''
+	};
+
+	transformObject(
+		entity,
+		`${node.id}  >>> JSON`,
+		'MetadataSchemaEntity');
 
 
 }
