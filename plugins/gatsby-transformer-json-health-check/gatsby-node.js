eb1c0e60471fe7c3904e832a1ae0e4f78f845eea
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2019-03-26 10:26:20 +1000

    System status JSON

--- plugins/gatsby-transformer-json-health-check/gatsby-node.js
+++ plugins/gatsby-transformer-json-health-check/gatsby-node.js
@@ -9,11 +9,11 @@ async function onCreateNode({node, getNode, actions, loadNodeContent}) {
 	const {createNode, createParentChildLink} = actions;
 
 	function transformObject(obj, id, type) {
-		const objStr = JSON.stringify(obj)
+		const objStr = JSON.stringify(obj);
 		const contentDigest = crypto
 			.createHash(`md5`)
 			.update(objStr)
-			.digest(`hex`)
+			.digest(`hex`);
 		const jsonNode = {
 			...obj,
 			id,
@@ -23,21 +23,24 @@ async function onCreateNode({node, getNode, actions, loadNodeContent}) {
 				contentDigest,
 				type,
 			},
-		}
-		createNode(jsonNode)
+		};
+		createNode(jsonNode);
 		createParentChildLink({parent: node, child: jsonNode})
 	}
-
 
 	// We only care about JSON content.
 	if (node.internal.mediaType !== `application/json`) {
 		return
 	}
 
+	// We only care about the health check JSON content
+	if (!node.relativePath.includes('config.json')) {
+		return
+	}
 
 	const content = await loadNodeContent(node);
 	const parsedContent = JSON.parse(content);
-
+	const systems = parsedContent.systems.map(s => s);
 
 	const relativeFilePath = createFilePath({
 		node,
@@ -45,51 +48,14 @@ async function onCreateNode({node, getNode, actions, loadNodeContent}) {
 		basePath: ''
 	});
 
-	function removeFileType(ref) {
-		return ref.split('.json')[0];
-	}
-
-	function splitRef(ref) {
-		let regex = /_/g;
-		let splitRef = ref.split('/');
-
-		return splitRef[splitRef.length - 1].split('.')[0].replace(regex, ' ');
-	}
-
-	const sections = relativeFilePath.split('/');
-
-	const propertyNames = _.keys(parsedContent.properties);
-	const requiredProperties = parsedContent.required || [];
-
-	const properties = propertyNames.map((name) => {
-		return {
-			name: name,
-			description: parsedContent.properties[name].description,
-			arrayModuleRef: parsedContent.properties[name].items ? parsedContent.properties[name].items.$ref ? removeFileType(parsedContent.properties[name].items.$ref) : '' : '',
-			arrayName: parsedContent.properties[name].items ? parsedContent.properties[name].items.$ref ? splitRef(parsedContent.properties[name].items.$ref) : '' : '',
-			arrayType: parsedContent.properties[name].items ? parsedContent.properties[name].items.type : '',
-			objectModuleRef: parsedContent.properties[name].$ref ? removeFileType(parsedContent.properties[name].$ref) : '',
-			objectName: parsedContent.properties[name].$ref ? splitRef(parsedContent.properties[name].$ref) : '',
-			required: requiredProperties.includes(name),
-			type: parsedContent.properties[name].type,
-			userFriendly: parsedContent.properties[name].user_friendly
-		}
-	});
-
-
-	const entity = {
-		title: parsedContent.title,
-		description: parsedContent.description,
-		properties: properties,
+	const healthCheck = {
+		systems: systems,
 		relativeFilePath: relativeFilePath,
-		schemaType: sections[1], // core, type or module
-		coreEntity: sections[2], // core type biomaterial, project,
-		category: sections[4] ? sections[3] : ''
 	};
 
 	transformObject(
-		entity,
-		`${node.id}  >>> HC`,
+		healthCheck,
+		`${node.id}  >>> JSON`,
 		'HealthCheck');
 
 
