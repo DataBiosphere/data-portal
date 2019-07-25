13eee9560a4db577975a22207b27833c91dab534
Fran McDade <frano-m@users.noreply.github.com>
2019-07-26 03:40:12 +1000

    Metadata Dictionary: Add copy to clipboard function to name and path on all entities. Resolves #435. (#459)

--- src/components/metadata/metadataDetail.js
+++ src/components/metadata/metadataDetail.js
@@ -7,6 +7,7 @@
 
 // Core dependencies
 import React from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 // App dependencies
 import HeadingTag from '../anchor/anchor';
@@ -20,34 +21,25 @@ const classNames = require('classnames');
 
 class MetadataDetail extends React.Component {
 
-	/**
-	 * Returns the unFriendly list separated by <span> for each unFriendly.
-	 * Allows wrapping of the list if required.
-	 * @param unFriendlyList
-	 * @returns {string}
-	 */
-	wrapByPeriod = (unFriendlyList) => {
-		return unFriendlyList.split('.').map((u, i) => {
-
-			if (i === 0) {
-				return `<span>${u}</span>`;
-			}
-			return `<span>.${u}</span>`;
-		}).join('');
-	};
-
 	render() {
 		const {anchor, exampleOrEnum, description, groupRef, isRequired, label, type, unFriendly} = this.props;
 		return (
 			<div className={classNames(compStyles.metadataDetail, {[compStyles.byGroup]: groupRef})}>
-					<span className={classNames({[fontStyles.xs]: !groupRef}, {[fontStyles.semiBold]: !groupRef}, {[fontStyles.l]: groupRef}, compStyles.title)}>{label}
-						{isRequired ? <span className={fontStyles.xs}>*</span> : null}{groupRef ? <span className={classNames(fontStyles.xxs, compStyles.type)}>({type})</span> : null}
+					<span
+						className={classNames({[fontStyles.xs]: !groupRef}, {[fontStyles.semiBold]: !groupRef}, {[fontStyles.l]: groupRef}, compStyles.title)}>{label}
+						{isRequired ? <span className={fontStyles.xs}>*</span> : null}{groupRef ?
+							<span className={classNames(fontStyles.xxs, compStyles.type)}>({type})</span> : null}
 						{anchor ? <HeadingTag anchor={anchor}/> : null}</span>
-				{unFriendly && !groupRef ? <span className={classNames(fontStyles.hcaCode, compStyles.unFriendly)}
-												 dangerouslySetInnerHTML={{__html: this.wrapByPeriod(unFriendly)}}/> : null}
+				{unFriendly && !groupRef ? <CopyToClipboard text={unFriendly}>
+					<span className={classNames(fontStyles.hcaCode, compStyles.unFriendly)}>
+						{unFriendly.split('.').map((u, i, a) =>
+							<span key={i}>{i !== 0 ? '.' : ''}{u}{i === a.length - 1 ? <i className='material-icons'>file_copy</i> : ''}</span>)}
+					</span>
+				</CopyToClipboard> : null}
 				{type && !groupRef ? <span
 					className={classNames(fontStyles.xxs, compStyles.type)}>{type}</span> : null}
-				{description ? <Linkify className={classNames(fontStyles.xs, compStyles.description)}>{description}</Linkify> : null}
+				{description ? <Linkify
+					className={classNames(fontStyles.xs, compStyles.description)}>{description}</Linkify> : null}
 				{exampleOrEnum ?
 					<span className={classNames(fontStyles.xs, compStyles.example)}>{exampleOrEnum}</span> : null}
 			</div>
