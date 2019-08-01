941fea855869d2e289736fe9d1cf7c9a329ee3ac
Fran McDade <frano-m@users.noreply.github.com>
2019-08-02 06:57:44 +1000

    Remove Beta banner from prod. Resolves #472. (#473)

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -34,14 +34,12 @@ class Banner extends React.Component {
 	};
 
 	render(props) {
-		let testMessage = 'This is a test environment and periodically may be unavailable and/or may contain test data.',
-			prodMessage = 'This is a beta test environment and periodically features, content, or data may change or be unavailable.';
+		let testMessage = 'This is a test environment and periodically may be unavailable and/or may contain test data.';
+			// prodMessage = 'This is a beta test environment and periodically features, content, or data may change or be unavailable.';
 		return (
 			<div className={this.getBannerClassName(this.props.position)}>
 				{this.props.position === 'top' ? this.props.healthy === false ? <SystemStatus/> : null : null}
-				{this.props.position === 'top' ? this.isTestEnvironment() ?
-					<Environment cookieName='environmentAccepted' message={testMessage}/> :
-					<Environment cookieName='prodEnvironmentAccepted' message={prodMessage}/> : null}
+				{this.props.position === 'top' && this.isTestEnvironment() ? <Environment cookieName='environmentAccepted' message={testMessage}/> : null}
 				{this.props.position === 'bottom' ? <Privacy cookieName='privacyAccepted'/> : null}
 			</div>
 		);
