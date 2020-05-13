c62adde014be539703273339f881cb23f1974e5f
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2020-05-13 10:33:45 +1000

    Add ability for description front matter field and copy to meta  twitter and og description tags if it exists. Resolves #679.

--- src/components/seo/seo.js
+++ src/components/seo/seo.js
@@ -9,9 +9,6 @@
 import Helmet from 'react-helmet';
 import React from 'react';
 
-// App dependencies
-import * as EnvironmentService from '../../utils/environment.service';
-
 class SEO extends React.Component {
 
 	render() {
@@ -20,17 +17,14 @@ class SEO extends React.Component {
 
 		return (
 			<Helmet>
-				<title>{title}</title>
-				{EnvironmentService.isProd() ? null : <meta name='robots' content='noindex'/>}
-				<link rel='stylesheet' href='https://fonts.googleapis.com/icon?family=Material+Icons'/>
-				<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700'/>
-				<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=PT+Mono'/>
-				<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Source+Code+Pro:300,400'/>
-				<link rel='stylesheet' href='https://use.typekit.net/qhb0geh.css'/>
 				<meta property='og:title' content={title}/>
 				<meta property='og:site_name' content='HCA Data Portal'/>
 				<meta property='twitter:title' content={title}/>
-				{description ? <meta property='twitter:description' content={description}/> : null}
+				{description ? [
+					<meta property='twitter:description' content={description} key='twitter:description'/>,
+					<meta name='description' content={description} key='description'/>,
+					<meta property='og:description' content={description} key='og:description'/>
+					] : null}
 				<meta name='twitter:card' content='summary'/>
 			</Helmet>
 		);
