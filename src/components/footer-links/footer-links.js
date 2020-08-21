eafd3e404ffc95fcea06de45ca3da2ede3228f6f
Fran McDade <franmcdade@Frans-MBP.hub>
2020-08-21 14:52:37 +1000

    Hide system status links. #721.

---
+++ src/components/footer-links/footer-links.js
@@ -0,0 +1,37 @@
+/*
+ * Human Cell Atlas
+ * https://www.humancellatlas.org/
+ *
+ * HCA Data Portal footer links component.
+ */
+
+// Core dependencies
+import React from "react";
+
+// App dependencies
+import FooterLink from "../footer-link/footer-link";
+import * as FooterService from "../../utils/footer.service";
+
+// Styles
+import compStyles from "./footer-links.module.css";
+
+class FooterLinks extends React.Component {
+
+    render() {
+        const {links} = this.props;
+        return (
+            <div className={compStyles.links}>
+                {links.map((link, l) => <FooterLink key={l} link={link}/>)}
+            </div>
+        );
+    }
+}
+
+export default () => {
+
+    const footerLinks = FooterService.getFooterLinks();
+
+    return (
+        <FooterLinks links={footerLinks}/>
+    )
+}
