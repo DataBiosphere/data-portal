e1f4bb1213a20cec2ea491d925adb6c43553ba91
MillenniumFalconMechanic <mim@clevercanary.com>
2020-09-23 09:30:10 -0700

    Added feedback form. Resolves #729.

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,52 +6,66 @@
  */
 
 // Core dependencies
-import React from 'react';
+import React from "react";
 
 // App dependencies
-import Banner from './banner/banner';
-import Footer from './footer/footer';
-import Header from './header/header';
-import HCAMain from './hcaMain/hcaMain';
+import Banner from "./banner/banner";
+import Footer from "./footer/footer";
+import Header from "./header/header";
+import HCAMain from "./hcaMain/hcaMain";
 import PageHead from "./pageHead/pageHead";
-import SEO from './seo/seo';
+import SEO from "./seo/seo";
+import SupportRequest from "./supportRequest/supportRequest";
 
 // Styles
-import compStyles from './layout.module.css';
+import compStyles from "./layout.module.css";
 
-let classNames = require('classnames');
+let classNames = require("classnames");
 
-require(`katex/dist/katex.min.css`);
+require("katex/dist/katex.min.css");
 
 class Layout extends React.Component {
 
-	constructor(props) {
-		super(props);
-		this.state = {noScroll: false};
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            noScroll: false,
+            supportRequestActive: false
+        };
+    }
 
-	onMenuOpen = (event) => {
-		this.setState({noScroll: !event});
-	};
+    onMenuOpen = (event) => {
+        this.setState({noScroll: !event});
+    };
 
-	render() {
-		const {children, description, docPath, healthy, homePage, homeTab, nav, pageTitle, sectionTitle, showAllMetadata} = this.props;
-		return (
-			<div>
-				<PageHead pageTitle={pageTitle}/>
-				<SEO description={description} pageTitle={pageTitle}/>
-				<div className={classNames(compStyles.site, {[compStyles.noScroll]: this.state.noScroll})}>
-					<Header onMenuOpen={this.onMenuOpen.bind(this)} homePage={homePage} docPath={docPath}/>
-					<Banner position={'top'} healthy={healthy}/>
-					{homePage ? children :
-						<HCAMain docPath={docPath} homeTab={homeTab} nav={nav}
-								 sectionTitle={sectionTitle} showAllMetadata={showAllMetadata}>{children}</HCAMain>}
-					<Banner position={'bottom'}/>
-					<Footer/>
-				</div>
-			</div>
-		)
-	}
+    onToggleSupportRequestForm = (active) => {
+        this.setState({supportRequestActive: active});
+    };
+
+    render() {
+        const {children, description, docPath, healthy, homePage, homeTab, nav, pageTitle, sectionTitle, showAllMetadata} = this.props;
+        const {noScroll, supportRequestActive} = this.state;
+        return (
+            <div>
+                <PageHead pageTitle={pageTitle}/>
+                <SEO description={description} pageTitle={pageTitle}/>
+                <div className={classNames(compStyles.site, {[compStyles.noScroll]: noScroll})}>
+                    <Header onMenuOpen={this.onMenuOpen.bind(this)} homePage={homePage} docPath={docPath}/>
+                    <Banner position={'top'} healthy={healthy}/>
+                    {homePage ? children :
+                        <HCAMain docPath={docPath} homeTab={homeTab} nav={nav}
+                                 sectionTitle={sectionTitle} showAllMetadata={showAllMetadata}>{children}</HCAMain>}
+                    {supportRequestActive ?
+                        <SupportRequest dismissFn={() => this.onToggleSupportRequestForm(false)}/> :
+                        <button className={compStyles.supportRequestBtn}
+                                onClick={() => this.onToggleSupportRequestForm(true)}>Feedback &amp; Support
+                        </button>}
+                    <Banner position={'bottom'}/>
+                    <Footer/>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Layout;
---
+++ src/components/supportRequest/supportRequestError.js
@@ -0,0 +1,33 @@
+/*
+ * Human Cell Atlas
+ * https://www.humancellatlas.org/
+ *
+ * Component displaying error message when attempting to submit Zendesk support request form.
+ */
+
+// Core dependencies
+import React from "react";
+
+// App dependencies
+import compStyles from "./supportRequestError.module.css";
+
+// Class name helper
+const classNames = require("classnames");
+
+class SupportRequestError extends React.Component {
+
+    render() {
+        // Error is displayed either at top of form as an overall submit error, or above an individual form element.
+        const {children, field} = this.props;
+        return (
+            <p className={classNames(
+                compStyles.supportRequestError, 
+                {[compStyles.field]: field},
+                {[compStyles.submit]: !field})}>
+                {children}
+            </p>
+        );
+    }
+}
+
+export default SupportRequestError;
