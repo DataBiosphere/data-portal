8407080193505f29cbea43477ebdd66a5d8a290f
MillenniumFalconMechanic <mim@clevercanary.com>
2020-09-23 09:30:10 -0700

    Added feedback form. Resolves #729.

---
+++ src/components/buttonLoader/buttonLoader.js
@@ -0,0 +1,21 @@
+/*
+ * Human Cell Atlas
+ * https://www.humancellatlas.org/
+ *
+ * Loader displayed inside standard Data Portal button.
+ */
+
+// Core dependencies
+import React from "react";
+
+// Styles
+import compStyles from "./buttonLoader.module.css"; 
+
+const ButtonLoader = () => {
+
+    return (
+        <div className={compStyles.buttonLoader}/>
+    );
+};
+
+export default ButtonLoader;
---
+++ src/components/supportRequest/supportRequestSubmitted.js
@@ -0,0 +1,26 @@
+/*
+ * Human Cell Atlas
+ * https://www.humancellatlas.org/
+ *
+ * Component displaying confirmation message from a successfully submitted Zendesk support request form.
+ */
+
+// Core dependencies
+import React from "react";
+
+// Styles
+import compStyles from "./supportRequestSubmitted.module.css";
+
+class SupportRequestSubmitted extends React.Component {
+
+    render() {
+        return (
+            <div className={compStyles.supportRequestSubmitted}>
+                <h2>Thank You</h2>
+                <p>Your request has been submitted.</p>
+            </div>
+        );
+    }
+}
+
+export default SupportRequestSubmitted;
