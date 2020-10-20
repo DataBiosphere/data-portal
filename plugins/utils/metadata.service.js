d098f70553b462c928b24bc3fc680dfb67383c45
noopdog <dave@clevercanary.com>
2020-10-20 11:05:18 -0700

    entity type text updated

--- plugins/utils/metadata.service.js
+++ plugins/utils/metadata.service.js
@@ -15,8 +15,8 @@ const denySchemaFilePaths = ["property_migrations.json", "versions.json"];
 const EntityDescription = {
     "CORE": "Core entities are those that apply to 100% of all entities and are highly stable. Example Core fields include those for IDs and accessions. Core fields are inherited by Type entities.",
     "MODULE": "Module entities contribute groups of fields with a specific theme to one or more type entities.",
-    "SYSTEM": "System entities ...",
-    "TYPE": "Type entities model the different classes of artifacts composing an experiment. Type entities ingerit properties from their corresponding core entity."
+    "SYSTEM": "System entities are created and populated by the Data Coordination Platform during data ingest and processing. ",
+    "TYPE": "Type entities represent different parts of an experiment: A biomaterial (e.g. a tissue sample) can undergo a process (e.g. dissociation) to produce another biomaterial (e.g. a sample of dissociated cells) or a set of data files (e.g. 10X fastq files). The process that was actually executed follows a specific protocol (e.g. a 10X protocol). All of these parts make up the overall project (e.g. Understanding cell types in the human heart). Type entities are composed of simple properties, Core entities, Module entities and System entities."
 };
 const metadataJsonPath = getMetadataJSONPath();
 const urlGitHubPrefix = "https://github.com/HumanCellAtlas/metadata-schema/blob/master/json_schema";
