b1b3c93337732a4bddebd3421580df2fa82ceb7b
MillenniumFalconMechanic <mim@clevercanary.com>
2020-10-29 15:07:52 -0700

    Minor linting. Resolves #767.

--- src/utils/release.service.js
+++ src/utils/release.service.js
@@ -5,9 +5,6 @@
  * Service coordinating release-related functionality. 
  */
 
-// App dependencies
-import * as EnvironmentService from "./environment/environment.service";
-
 /**
  * Returns true if release is visible for the current environment; not currently visible in any environment.
  */
