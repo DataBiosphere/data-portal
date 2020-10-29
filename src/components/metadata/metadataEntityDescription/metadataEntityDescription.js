db1adf7553330e72d5b848888cbb7749e733c77e
noopdog <dave@clevercanary.com>
2020-10-28 22:59:56 -0700

    updated entity description text

--- src/components/metadata/metadataEntityDescription/metadataEntityDescription.js
+++ src/components/metadata/metadataEntityDescription/metadataEntityDescription.js
@@ -6,7 +6,10 @@
  */
 
 // Core dependencies
+import Link from "gatsby-link";
 import React from "react";
+
+
 
 function MetadataEntityDescription(props) {
 
@@ -16,13 +19,13 @@ function MetadataEntityDescription(props) {
     return (
         <>
         {entityName === "core" ?
-            <p>Core entities are those that apply to 100% of all entities and are highly stable. Example Core fields include those for IDs and accessions. Core fields are inherited by Type entities.</p> : null}
+            <div><p>Core entities are those that apply to 100% of all Type entities and are highly stable. For example, Core fields include those for IDs and accessions. Each Type entity includes a single Core entity thereby inheriting the Core entity's fields.</p></div> : null}
         {entityName === "module" ?
-            <p>Module entities contribute groups of fields with a specific theme to one or more type entities.</p> : null}
+            <div><p>Module entities contribute groups of fields with a specific theme to one or more type entities.</p></div> : null}
         {entityName === "system" ?
-            <p>System entities are created and populated by the Data Coordination Platform during data ingest and processing.</p> : null}
+            <div><p>System entities are created and populated by the Data Coordination Platform during data ingest and processing.</p></div> : null}
         {entityName === "type" ?
-            <p>Type entities represent different parts of an experiment: A biomaterial (e.g. a tissue sample) can undergo a process (e.g. dissociation) to produce another biomaterial (e.g. a sample of dissociated cells) or a set of data files (e.g. 10X fastq files). The process that was actually executed follows a specific protocol (e.g. a 10X protocol). All of these parts make up the overall project (e.g. Understanding cell types in the human heart). Type entities are composed of simple properties, Core entities, Module entities and System entities.</p> : null}
+            <div><p>Type entities represent different parts of an experiment. For example, a <strong>biomaterial</strong> (e.g. a <Link to={"/metadata/dictionary/biomaterial/specimen_from_organism"}>tissue sample</Link>) can undergo a <strong>process</strong> (e.g. <Link to={"/metadata/dictionary/process/process"}>dissociation</Link>) to produce another biomaterial (e.g. a <Link to={"/metadata/dictionary/biomaterial/cell_suspension"}>sample of dissociated cells</Link>) or a set of data <strong>files</strong> (e.g. <Link to={"/metadata/dictionary/file/sequence_file"}>10X fastq files</Link>).</p> <p>The process that was actually executed follows a specific <strong>protocol</strong> (e.g. a <Link to={"/metadata/dictionary/protocol/sequencing_protocol"}>10X protocol</Link>). All of these parts make up the overall <Link to={"/metadata/dictionary/project/project"}>project</Link> (e.g. Understanding cell types in the human heart).</p> <p>To enable reuse of common schema structures, Type entities are composed of <Link to={"/metadata/dictionary/biomaterial/biomaterial_core"}>Core entities</Link>, <Link to={"/metadata/dictionary/biomaterial/cell_morphology"}>Module entities</Link> and <Link to={"/metadata/dictionary/system/links"}>System entities</Link>.</p> <p> Every Type entity includes a single <Link to={"/metadata/dictionary/biomaterial/biomaterial_core"}>Core entity</Link> from which it inherits properties common to its core type (e.g. Biomaterial, Process, Protocol, File or Project). Type entities also include at least one <Link to={"/metadata/dictionary/system/links"}>System entity</Link> (<Link to={"/metadata/dictionary/system/provenance"}>Provenance</Link>) and zero or more <Link to={"/metadata/dictionary/biomaterial/cell_morphology"}>Module entities</Link>. See the <Link to={"/metadata/structure"}>metadata structure</Link> documentation for additional detail.</p></div> : null}
         </>
     );
 }
