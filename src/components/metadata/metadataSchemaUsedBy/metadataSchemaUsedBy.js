755dc6448cfff73e74c0d00c04a8833ecdbcf99e
Fran McDade <franmcdade@Frans-MBP.hub>
2020-11-27 12:51:29 +1000

    Make Metadata Dictionary Searchable. UI linting. Component cleanup and refactor for component reuse. #260.

--- src/components/metadata/metadataSchemaUsedBy/metadataSchemaUsedBy.js
+++ src/components/metadata/metadataSchemaUsedBy/metadataSchemaUsedBy.js
@@ -9,14 +9,12 @@
 import React, {useContext} from "react";
 
 // App dependencies
-import ClickHandler from "../../clickHandler/clickHandler";
 import ContextMetadataDisplaying from "../contextMetadataDisplaying/contextMetadataDisplaying";
 import MetadataSchemaPropertyFieldDescription from "../metadataSchemaPropertyFieldDescription/metadataSchemaPropertyFieldDescription";
 import MetadataSchemaPropertyFieldFriendlies from "../metadataSchemaPropertyFieldFriendlies/metadataSchemaPropertyFieldFriendlies";
+import MetadataSchemaPropertyPanel from "../metadataSchemaPropertyPanel/metadataSchemaPropertyPanel";
+import MetadataSchemaPropertyPanelBundle from "../metadataSchemaPropertyPanelBundle/metadataSchemaPropertyPanelBundle";
 import MetadataSchemaPropertyWordWrapper from "../metadataSchemaPropertyWordWrapper/metadataSchemaPropertyWordWrapper";
-
-// Styles
-import compStyles from "./metadataSchemaUsedBy.module.css";
 
 function MetadataSchemaUsedBy(props) {
 
@@ -25,17 +23,15 @@ function MetadataSchemaUsedBy(props) {
     const {onHandleNavigationHit} = useContext(ContextMetadataDisplaying);
 
     return (
-        <ClickHandler className={compStyles.usedBy}
-                      clickAction={() => onHandleNavigationHit(property)}
-                      tag={"div"}>
-            <span>
+        <MetadataSchemaPropertyPanel hover onHandleClick={() => onHandleNavigationHit(property)}>
+            <MetadataSchemaPropertyPanelBundle>
                 <MetadataSchemaPropertyFieldFriendlies property={property}/>
-            </span>
-            <span>
+            </MetadataSchemaPropertyPanelBundle>
+            <MetadataSchemaPropertyPanelBundle>
                 <MetadataSchemaPropertyWordWrapper font={"hcaCode"} word={propertyPath} wrap/>
                 <MetadataSchemaPropertyFieldDescription font={"xs"} property={property}/>
-            </span>
-        </ClickHandler>
+            </MetadataSchemaPropertyPanelBundle>
+        </MetadataSchemaPropertyPanel>
     )
 }
 
