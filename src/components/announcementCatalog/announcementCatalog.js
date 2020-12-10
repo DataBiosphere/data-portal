07af6e7b87e7b6c57d3dcd6627e9d318056b4cf7
MillenniumFalconMechanic <mim@clevercanary.com>
2020-12-09 17:23:11 -0800

    Added tracking of view DCP2 preview. Resolves #807.

--- src/components/announcementCatalog/announcementCatalog.js
+++ src/components/announcementCatalog/announcementCatalog.js
@@ -14,8 +14,19 @@ import Announcement from "../announcement/announcement";
 
 // Styles
 import compStyles from "./announcementCatalog.module.css";
+import * as DPGTMService from "../../utils/dp-gtm/dp-gtm.service";
 
 function AnnouncementCatalog() {
+
+    /**
+     * Track click on view catalog.
+     *
+     * @param catalog
+     */
+    const trackCatalogViewed = (catalog) => {
+
+        DPGTMService.trackCatalogViewed(catalog);
+    };
 
     const dcp2ComingSoonPath = "/what-is-the-dcp-20-data-preview";
     const dcp2PreviewPath = `${process.env.GATSBY_EXPLORE_URL}projects?catalog=dcp2`;
@@ -24,7 +35,7 @@ function AnnouncementCatalog() {
             <span className={compStyles.catalog}>
                 <span><strong>Update: </strong>A preview of the HCA DCP 2.0 data is now available.</span>
                 <span>
-                    <a href={dcp2PreviewPath}>View DCP 2.0 Data Preview</a>
+                    <a href={dcp2PreviewPath} onClick={() => trackCatalogViewed('dcp2')}>View DCP 2.0 Data Preview</a>
                     <span>|</span>
                     <Link to={dcp2ComingSoonPath}>Learn More</Link>
                 </span>
