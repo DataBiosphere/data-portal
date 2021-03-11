ff1f8575c020092c1a6a0b9724378b883b4dac76
Mim Hastie <mim@clevercanary.com>
2021-03-11 10:51:20 -0800

    Encoded DB filter params. Resolves #846. (#876)

--- src/components/explore/exploreControls.js
+++ src/components/explore/exploreControls.js
@@ -24,212 +24,216 @@ let classNames = require('classnames');
 /* List of organ term names for display */
 /* Excludes "development samples", "gut", "female reproductive", "male reproductive" */
 let organTermList = [
-	"blood",
-	"kidney",
-	"bone tissue",
-	"liver",
-	"brain",
-	"lung",
-	"pancreas",
-	"heart",
-	"immune system",
-	"skin of body"
+    "blood",
+    "kidney",
+    "bone tissue",
+    "liver",
+    "brain",
+    "lung",
+    "pancreas",
+    "heart",
+    "immune system",
+    "skin of body"
 ];
 
 class ExploreControls extends React.Component {
 
-	constructor(props) {
-		super(props);
+    constructor(props) {
+        super(props);
 
-		this.state = {activeOrgan: ""};
-		this.setOrganActive = this.setOrganActive.bind(this);
-	}
+        this.state = {activeOrgan: ""};
+        this.setOrganActive = this.setOrganActive.bind(this);
+    }
 
-	componentDidMount() {
-		this.setOrganInteractions();
-	}
+    componentDidMount() {
+        this.setOrganInteractions();
+    }
 
-	componentWillUnmount() {
-		this.removeOrganInteractions();
-	};
+    componentWillUnmount() {
+        this.removeOrganInteractions();
+    };
 
-	clearOrganActive = () => {
+    clearOrganActive = () => {
 
-		return () => {
+        return () => {
 
-			this.setState({activeOrgan: ""});
-			this.props.onActiveOrgan("");
-		}
-	};
+            this.setState({activeOrgan: ""});
+            this.props.onActiveOrgan("");
+        }
+    };
 
-	getCellCount = () => {
+    getCellCount = () => {
 
-		const {totalCellCount} = this.props;
-		const activeOrgan = this.state.activeOrgan;
+        const {totalCellCount} = this.props;
+        const activeOrgan = this.state.activeOrgan;
 
-		if ( activeOrgan === "" ) {
+        if ( activeOrgan === "" ) {
 
-			return totalCellCount; // No organ is active, show total cell count
-		}
-		else if ( !this.isOrganSummarized(activeOrgan) ) {
+            return totalCellCount; // No organ is active, show total cell count
+        }
+        else if ( !this.isOrganSummarized(activeOrgan) ) {
 
-			return "00"; // For any organ not summarized by browser
-		}
-		else {
+            return "00"; // For any organ not summarized by browser
+        }
+        else {
 
-			// Organ is summarized and will return a cell count value
-			return numberFormatter.formatCount(this.getOrganFilter(activeOrgan)[0].cellCount);
-		}
-	};
+            // Organ is summarized and will return a cell count value
+            return numberFormatter.formatCount(this.getOrganFilter(activeOrgan)[0].cellCount);
+        }
+    };
 
-	getCellCountLabel = () => {
+    getCellCountLabel = () => {
 
-		const activeOrgan = this.state.activeOrgan;
+        const activeOrgan = this.state.activeOrgan;
 
-		if ( activeOrgan === "" ) {
+        if ( activeOrgan === "" ) {
 
-			return "All"; // No organ is active
-		}
-		else {
+            return "All"; // No organ is active
+        }
+        else {
 
-			return this.getOrganDisplayLabel(activeOrgan); // Show display label for active organ
-		}
-	};
+            return this.getOrganDisplayLabel(activeOrgan); // Show display label for active organ
+        }
+    };
 
-	getOrganDisplayLabel = (organ) => {
+    getOrganDisplayLabel = (organ) => {
 
-		const displayLabel = this.translateOrganNameToDisplayLabel(organ); // Converts any organ to corresponding display label (if specified)
+        const displayLabel = this.translateOrganNameToDisplayLabel(organ); // Converts any organ to corresponding display label (if specified)
 
-		return stringFormatter.convertSentenceCaseToTitleCase(displayLabel);
-	};
+        return stringFormatter.convertSentenceCaseToTitleCase(displayLabel);
+    };
 
-	getOrganEls = () => {
+    getOrganEls = () => {
 
-		return document.querySelectorAll("[id^=organ]");
-	};
+        return document.querySelectorAll("[id^=organ]");
+    };
 
-	getOrganFilter = (organ) => {
+    getOrganFilter = (organ) => {
 
-		const {cellCountSummaries} = this.props;
+        const {cellCountSummaries} = this.props;
 
-		if ( !cellCountSummaries || cellCountSummaries.length === 0 ) {
+        if ( !cellCountSummaries || cellCountSummaries.length === 0 ) {
 
-			return []; // For when cell count summary is being fetched, or empty
-		}
+            return []; // For when cell count summary is being fetched, or empty
+        }
 
-		return cellCountSummaries.filter(cellCountSummary => cellCountSummary.label === organ);
-	};
+        return cellCountSummaries.filter(cellCountSummary => cellCountSummary.label === organ);
+    };
 
-	getOrganId = (organ) => {
+    getOrganId = (organ) => {
 
-		const idStem = stringFormatter.convertSentenceCaseToTitleCase(organ).replace(/\s/g, "");
+        const idStem = stringFormatter.convertSentenceCaseToTitleCase(organ).replace(/\s/g, "");
 
-		return `organ${idStem}`;
-	};
+        return `organ${idStem}`;
+    };
 
-	getOrganIdStem = (organId) => {
+    getOrganIdStem = (organId) => {
 
-		return organId.split("organ")[1]; // Reverts organ id to organ id stem
-	};
+        return organId.split("organ")[1]; // Reverts organ id to organ id stem
+    };
 
-	isOrganSummarized = (organ) => {
+    isOrganSummarized = (organ) => {
 
-		return this.getOrganFilter(organ).length > 0; // Returns true when the specified organ is listed in the summary
-	};
+        return this.getOrganFilter(organ).length > 0; // Returns true when the specified organ is listed in the summary
+    };
 
-	removeOrganInteractions = () => {
+    removeOrganInteractions = () => {
 
-		const organEls = this.getOrganEls();
+        const organEls = this.getOrganEls();
 
-		// Remove event listener for mouse over and mouse out and click event of organ elements
-		organEls.forEach(organEl => {
+        // Remove event listener for mouse over and mouse out and click event of organ elements
+        organEls.forEach(organEl => {
 
-			organEl.removeEventListener('mouseenter', this.setOrganActive(organEl));
-			organEl.removeEventListener('mouseleave', this.clearOrganActive());
-		});
-	};
+            organEl.removeEventListener('mouseenter', this.setOrganActive(organEl));
+            organEl.removeEventListener('mouseleave', this.clearOrganActive());
+        });
+    };
 
-	setOrganActive = (organEl) => {
+    setOrganActive = (organEl) => {
 
-		return () => {
+        return () => {
 
-			const organId = this.getOrganIdStem(organEl.getAttribute("id"));
-			const organLabel = stringFormatter.convertCamelCasetoTitleCase(organId).toLowerCase().trim();
+            const organId = this.getOrganIdStem(organEl.getAttribute("id"));
+            const organLabel = stringFormatter.convertCamelCasetoTitleCase(organId).toLowerCase().trim();
 
-			this.setState({activeOrgan: organLabel});
-			this.props.onActiveOrgan(organId);
-		}
-	};
+            this.setState({activeOrgan: organLabel});
+            this.props.onActiveOrgan(organId);
+        }
+    };
 
-	setOrganInteractions = () => {
+    setOrganInteractions = () => {
 
-		const organEls = this.getOrganEls();
+        const organEls = this.getOrganEls();
 
-		// Add event listener for mouse over and mouse out and click event of organ elements
-		organEls.forEach(organEl => {
+        // Add event listener for mouse over and mouse out and click event of organ elements
+        organEls.forEach(organEl => {
 
-			organEl.addEventListener('mouseenter', this.setOrganActive(organEl));
-			organEl.addEventListener('mouseleave', this.clearOrganActive());
-		});
-	};
+            organEl.addEventListener("mouseenter", this.setOrganActive(organEl));
+            organEl.addEventListener("mouseleave", this.clearOrganActive());
+        });
+    };
 
-	stringifyOrganFilter = (term) => {
+    stringifyOrganFilter = (term) => {
 
-		return JSON.stringify([{"facetName": "specimenOrgan", "terms": [term]}]);
-	};
+        return JSON.stringify([{"facetName": "specimenOrgan", "terms": [term]}]);
+    };
 
-	translateOrganNameToDisplayLabel = (organName) => {
+    translateOrganNameToDisplayLabel = (organName) => {
 
-		switch (organName) {
-			case "bone tissue":
-				return "bone";
-			case "female reproductive":
-				return "Reproductive (Female)";
-			case "male reproductive":
-				return "Reproductive (Male)";
-			case "skin of body":
-				return "skin";
-			default:
-				return organName;
-		}
-	};
+        switch ( organName ) {
+            case "bone tissue":
+                return "bone";
+            case "female reproductive":
+                return "Reproductive (Female)";
+            case "male reproductive":
+                return "Reproductive (Male)";
+            case "skin of body":
+                return "skin";
+            default:
+                return organName;
+        }
+    };
 
-	visitExploreLink = (organ) => {
+    visitExploreLink = (organ) => {
 
-		if ( !this.isOrganSummarized(organ) ) {
+        if ( !this.isOrganSummarized(organ) ) {
 
-			return; // Corresponding organ is not summarised by cell count summary
-		}
+            return; // Corresponding organ is not summarised by cell count summary
+        }
 
-		const organFilter = this.getOrganFilter(organ)[0];
-		const stringifyOrganFilter = this.stringifyOrganFilter(organFilter.label);
+        const organFilter = this.getOrganFilter(organ)[0];
+        const stringifyOrganFilter = this.stringifyOrganFilter(organFilter.label);
 
-		window.location.href = `${process.env.GATSBY_EXPLORE_URL}projects?filter=${stringifyOrganFilter}`;
-	};
+        const params = new URLSearchParams();
+        params.set("filter", stringifyOrganFilter);
+        window.location.href = `${process.env.GATSBY_EXPLORE_URL}projects?${params.toString()}`;
+    };
 
-	render() {
-		const {cellCountSummaries} = this.props;
-		return (
-			<div className={compStyles.controls}>
-				<div className={classNames({[compStyles.loading]: !cellCountSummaries})}>
-					<span className={compStyles.count}>{this.getCellCount()} Cells</span>
-					<span className={compStyles.label}>{this.getCellCountLabel()} Cells</span>
-				</div>
-				<div className={compStyles.organs}>
-					{organTermList ? organTermList.map((organ, i) =>
-						<ClickHandler
-							className={classNames(compStyles.organ, {[compStyles.unspecified]: !this.isOrganSummarized(organ)})}
-							clickAction={() => this.visitExploreLink(organ)}
-							id={this.getOrganId(organ)}
-							key={i}
-							tag={'div'}>
-							<span>{this.getOrganDisplayLabel(organ)}</span>
-							<img src={arrow} alt="Select"/></ClickHandler>) : null}
-				</div>
-				<a href={process.env.GATSBY_EXPLORE_URL} className={compStyles.moreOrgans}>View More Organs</a>
-			</div>
-		);
-	}
+    render() {
+        const {cellCountSummaries} = this.props;
+        return (
+            <div className={compStyles.controls}>
+                <div className={classNames({[compStyles.loading]: !cellCountSummaries})}>
+                    <span className={compStyles.count}>{this.getCellCount()} Cells</span>
+                    <span className={compStyles.label}>{this.getCellCountLabel()} Cells</span>
+                </div>
+                <div className={compStyles.organs}>
+                    {organTermList ? organTermList.map((organ, i) =>
+                        <ClickHandler
+                            className={classNames(compStyles.organ,
+                                {[compStyles.unspecified]: !this.isOrganSummarized(organ)})}
+                            clickAction={() => this.visitExploreLink(organ)}
+                            id={this.getOrganId(organ)}
+                            key={i}
+                            tag={'div'}>
+                            <span>{this.getOrganDisplayLabel(organ)}</span>
+                            <img src={arrow} alt="Select"/>
+                        </ClickHandler>) : null}
+                </div>
+                <a href={process.env.GATSBY_EXPLORE_URL} className={compStyles.moreOrgans}>View More Organs</a>
+            </div>
+        );
+    }
 }
 
 export default ExploreControls;
