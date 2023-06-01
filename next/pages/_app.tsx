9826c7dc28b79d12e26fa3cae211541f202ea37c
Bruce Rodrigues <brucerodrigues6@gmail.com>
2023-05-31 21:50:42 -0300

    feat: adding config for header and footer #18 (#2127)
    * feat: adding config for header and footer #18
    
    * feat: changing dependencies versions and formating project #18
    
    * Update next/site-config/hca-dcp/config.ts
    
    Updated primary dark palette.
    
    * Update next/site-config/hca-dcp/config.ts
    
    Updated primary main palette
    
    ---------
    
    Co-authored-by: Fran McDade <frano-m@users.noreply.github.com>

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -1,8 +1,32 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppLayout } from "@clevercanary/data-explorer-ui/lib/components/Layout/components/AppLayout/appLayout.styles";
+import { ContentLayout } from "@clevercanary/data-explorer-ui/lib/components/Layout/components/ContentLayout/contentLayout";
+import { Footer } from "@clevercanary/data-explorer-ui/lib/components/Layout/components/Footer/footer";
+import { Header } from "@clevercanary/data-explorer-ui/lib/components/Layout/components/Header/header";
+import { Main } from "@clevercanary/data-explorer-ui/lib/components/Layout/components/Main/main.styles";
+import { createAppTheme } from "@clevercanary/data-explorer-ui/lib/theme/theme";
+import { ThemeProvider as EmotionThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import type { AppProps } from "next/app";
+import config from "../site-config/hca-dcp/config";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { themeOptions, layout } = config;
+  const theme = createAppTheme(themeOptions);
+  return (
+    <EmotionThemeProvider theme={theme}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppLayout>
+          <Header {...layout.header} />
+          <Main>
+            <ContentLayout content={<Component {...pageProps} />} />
+          </Main>
+          <Footer {...layout.footer} />
+        </AppLayout>
+      </ThemeProvider>
+    </EmotionThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ next/site-config/hca-dcp/header.ts
@@ -0,0 +1,53 @@
+import { ELEMENT_ALIGNMENT } from "@clevercanary/data-explorer-ui/lib/common/entities";
+import { HeaderProps } from "@clevercanary/data-explorer-ui/lib/components/Layout/components/Header/header";
+import logoHca from "images/logoHca.png";
+import { BROWSER_URL, LOGO_ALT, socials } from "./constants";
+
+const header: HeaderProps = {
+  authenticationEnabled: false,
+  logo: { alt: LOGO_ALT, link: BROWSER_URL, src: logoHca, height: 32 },
+  navAlignment: ELEMENT_ALIGNMENT.LEFT,
+  navLinks: [
+    {
+      label: "Explore",
+      url: "/",
+    },
+    {
+      label: "BioNetworks",
+      url: "/bio-networks",
+    },
+    {
+      label: "Guides",
+      url: `${BROWSER_URL}/guides`,
+    },
+    {
+      label: "Metadata",
+      url: `${BROWSER_URL}/metadata`,
+    },
+    {
+      label: "Pipelines",
+      url: `${BROWSER_URL}/pipelines`,
+    },
+    {
+      label: "Analysis Tools",
+      url: `${BROWSER_URL}/analyze`,
+    },
+    {
+      label: "Contribute",
+      url: `${BROWSER_URL}/contribute`,
+    },
+    {
+      label: "APIs",
+      url: `${BROWSER_URL}/apis`,
+    },
+    {
+      label: "Updates",
+      url: `${BROWSER_URL}/dcp-updates`,
+    },
+  ],
+  searchEnabled: true,
+  searchURL: `${BROWSER_URL}/search`,
+  socials,
+};
+
+export default header;
