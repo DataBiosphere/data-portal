8c214fb4138d3315de0c1043b64c5e89a96d22fa
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2023-06-16 16:51:54 +1000

    WIP

---
+++ next/next-sitemap.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import("next-sitemap").IConfig} */
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+
+const SEPARATOR = ",";
+
+const siteMapConfig = {
+  changefreq: "monthly",
+  exclude: (process.env.SITEMAP_EXCLUDE ?? "").split(SEPARATOR),
+  generateIndexSitemap: false,
+  outDir: `./out${basePath}`,
+  siteUrl: `${process.env.NEXT_PUBLIC_SITEMAP_DOMAIN}${basePath}`,
+};
+
+export default siteMapConfig;
