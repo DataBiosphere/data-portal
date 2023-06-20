d188cede2603f4eedf89e950340fc5fc0341bf22
Fran McDade <frano-m@users.noreply.github.com>
2023-06-21 04:13:51 +1000

    feat: implement "Stay Up-to-date" section in new nextjs homepage (#2145) (#2153)
    Co-authored-by: Fran McDade <franmcdade@Frans-MacBook-Pro.local>

---
+++ next/components/Home/components/SectionStayUpToDate/sectionStayUpToDate.tsx
@@ -0,0 +1,22 @@
+import { ButtonSecondary } from "@clevercanary/data-explorer-ui/lib/components/common/Button/components/ButtonSecondary/buttonSecondary";
+import { SectionHead } from "../Section/section.styles";
+import {
+  CTAs,
+  SectionContent,
+  SectionStayUpToDate as Section,
+} from "./sectionStayUpToDate.styles";
+
+export const SectionStayUpToDate = (): JSX.Element => {
+  return (
+    <Section>
+      <SectionContent>
+        <SectionHead>Stay up-to-date with the human cell atlas</SectionHead>
+        <CTAs>
+          <ButtonSecondary href="https://www.humancellatlas.org/join-hca/">
+            Join The HCA Register Of Interest
+          </ButtonSecondary>
+        </CTAs>
+      </SectionContent>
+    </Section>
+  );
+};
--- next/views/HomeView/homeView.tsx
+++ next/views/HomeView/homeView.tsx
@@ -1,11 +1,13 @@
 import { SectionBioNetworkAtlases } from "../../components/Home/components/SectionBioNetworkAtlases/sectionBioNetworkAtlases";
 import { SectionHero } from "../../components/Home/components/SectionHero/sectionHero";
+import { SectionStayUpToDate } from "../../components/Home/components/SectionStayUpToDate/sectionStayUpToDate";
 
 export const HomeView = (): JSX.Element => {
   return (
     <>
       <SectionHero />
       <SectionBioNetworkAtlases />
+      <SectionStayUpToDate />
     </>
   );
 };
