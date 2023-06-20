116525cba31c1f5fc8bc015235bfb0299a10775e
Mim Hastie <mim@clevercanary.com>
2023-06-20 15:56:43 -0700

    feat: atlas ga4 #2155 (#2156)

--- next/site-config/data-portal/prod/config.ts
+++ next/site-config/data-portal/prod/config.ts
@@ -5,4 +5,12 @@ const config: SiteConfig = {
   ...devConfig,
 };
 
+// Update gtmAuth for the prod environment lookup.
+if (config.analytics) {
+  const analytics = { ...config.analytics };
+  analytics.gtmAuth = "xm3qglWPEFim7Lb4AxXnsA";
+  analytics.gtmPreview = "env-2";
+  config.analytics = analytics;
+}
+
 export default config;
