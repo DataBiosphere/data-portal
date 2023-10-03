987e942f2ff6f1dc53bbe648025ebfacf01ca569
Fran McDade <frano-m@users.noreply.github.com>
2023-10-04 09:30:30 +1000

    fix: biological networks page layout (#2191) (#2193)

--- next/components/BioNetworks/components/Index/components/MainColumn/mainColumn.tsx
+++ next/components/BioNetworks/components/Index/components/MainColumn/mainColumn.tsx
@@ -1,4 +1,5 @@
 import { FluidPaper } from "@clevercanary/data-explorer-ui/lib/components/common/Paper/paper.styles";
+import { BackPageContentSingleColumn } from "@clevercanary/data-explorer-ui/lib/components/Layout/components/BackPage/backPageView.styles";
 import React from "react";
 import { useNetworkList } from "../../../../../../hooks/useNetworkList";
 import { getBioNetworksTableColumns } from "../../../../../../viewModelBuilders/viewModelBuilders";
@@ -7,13 +8,15 @@ import { DetailViewTable } from "./mainColumn.styles";
 export const MainColumn = (): JSX.Element => {
   const networks = useNetworkList();
   return (
-    <DetailViewTable
-      columns={getBioNetworksTableColumns()}
-      gridTemplateColumns="1fr max-content"
-      items={networks}
-      noResultsTitle={"No Networks"}
-      Paper={FluidPaper}
-      tools={null}
-    />
+    <BackPageContentSingleColumn>
+      <DetailViewTable
+        columns={getBioNetworksTableColumns()}
+        gridTemplateColumns="1fr max-content"
+        items={networks}
+        noResultsTitle={"No Networks"}
+        Paper={FluidPaper}
+        tools={null}
+      />
+    </BackPageContentSingleColumn>
   );
 };
