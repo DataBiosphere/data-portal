c65d863701fb33b61b4dd3d4ad9ee656bdd17995
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2023-10-18 16:39:33 +1000

    feat: update home page explore atlases hero UI (#2204)

--- next/components/Home/components/SectionBioNetworkAtlases/sectionBioNetworkAtlases.tsx
+++ next/components/Home/components/SectionBioNetworkAtlases/sectionBioNetworkAtlases.tsx
@@ -1,42 +1,44 @@
 import { StaticImage } from "@clevercanary/data-explorer-ui/lib/components/common/StaticImage/staticImage";
-import { Link } from "@clevercanary/data-explorer-ui/lib/components/Links/components/Link/link";
 import { TEXT_BODY_LARGE_500 } from "@clevercanary/data-explorer-ui/lib/theme/common/typography";
-import { Typography } from "@mui/material";
+import { Tooltip, Typography } from "@mui/material";
+import { useRouter } from "next/router";
 import { NETWORK_ICONS } from "../../../../constants/networks";
 import { NETWORKS_ROUTE } from "../../../../constants/routes";
 import { useNetworkList } from "../../../../hooks/useNetworkList";
 import { getBioNetworkName } from "../../../../viewModelBuilders/viewModelBuilders";
 import { Section, SectionHead } from "../Section/section.styles";
 import {
+  Badge,
   BioNetworkAtlas,
   BioNetworkAtlases,
   SectionContent,
 } from "./sectionBioNetworkAtlases.styles";
 
 export const SectionBioNetworkAtlases = (): JSX.Element => {
+  const router = useRouter();
   const networks = useNetworkList();
   return (
     <Section>
       <SectionContent>
         <SectionHead>Explore BioNetwork Atlases</SectionHead>
         <BioNetworkAtlases>
-          {networks.map(({ key, name, path }) => (
-            <Link
+          {networks.map(({ atlases, key, name, path }) => (
+            <BioNetworkAtlas
               key={key}
-              label={
-                <BioNetworkAtlas>
-                  <StaticImage
-                    alt={name}
-                    height={36}
-                    src={NETWORK_ICONS[key]}
-                  />
-                  <Typography variant={TEXT_BODY_LARGE_500}>
-                    {getBioNetworkName(name)}
-                  </Typography>
-                </BioNetworkAtlas>
+              onClick={(): Promise<boolean> =>
+                router.push(`${NETWORKS_ROUTE}/${path}`)
               }
-              url={`${NETWORKS_ROUTE}/${path}`}
-            />
+            >
+              <StaticImage alt={name} height={36} src={NETWORK_ICONS[key]} />
+              <Typography variant={TEXT_BODY_LARGE_500}>
+                {getBioNetworkName(name)}
+              </Typography>
+              {atlases.length > 0 && (
+                <Tooltip arrow placement="top" title="Atlases">
+                  <Badge>{atlases.length}</Badge>
+                </Tooltip>
+              )}
+            </BioNetworkAtlas>
           ))}
         </BioNetworkAtlases>
       </SectionContent>
