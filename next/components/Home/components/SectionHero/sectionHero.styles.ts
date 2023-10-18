95a5693a031a92b37ebfe3a8135e3baa81c341bb
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2023-10-18 11:32:42 +1000

    feat: update home page hero UI (#2205)

--- next/components/Home/components/SectionHero/sectionHero.styles.ts
+++ next/components/Home/components/SectionHero/sectionHero.styles.ts
@@ -1,3 +1,5 @@
+import { mediaTabletUp } from "@clevercanary/data-explorer-ui/lib/styles/common/mixins/breakpoints";
+import { white } from "@clevercanary/data-explorer-ui/lib/styles/common/mixins/colors";
 import { textBodyLarge400 } from "@clevercanary/data-explorer-ui/lib/styles/common/mixins/fonts";
 import {
   alpha32,
@@ -9,34 +11,43 @@ export const SectionHero = styled.section`
   background: url("/bio-networks/home/cell.png") no-repeat;
   background-position-x: center;
   background-size: cover;
-  color: ${({ theme }) => theme.palette.common.white};
+  color: ${white};
   width: 100%;
 `;
 
 export const Headline = styled.div`
   display: grid;
   justify-content: center;
-  margin: 72px 0;
+  margin: 56px 0 44px;
   padding: 0 16px;
+
+  ${mediaTabletUp} {
+    margin: 84px 0 104px;
+  }
 `;
 
 export const Head = styled.h1`
   font-family: "din-2014", sans-serif;
-  font-size: 48px;
+  font-size: 40px;
   font-weight: 400; // TODO: Update to 600 when font is available.
   letter-spacing: -0.4px;
-  line-height: 56px;
+  line-height: 48px;
   margin: 0;
   text-align: center;
 
   span {
     display: block;
   }
+
+  ${mediaTabletUp} {
+    font-size: 48px;
+    line-height: 56px;
+  }
 `;
 
 export const Subhead = styled.h2`
   ${textBodyLarge400};
-  margin: 8px 0 32px;
+  margin: 8px 0 24px;
   text-align: center;
 `;
 
@@ -46,13 +57,11 @@ export const CTAs = styled.div`
   justify-self: center;
 
   & .MuiButton-outlinedSecondary {
-    color: ${({ theme }) => theme.palette.common.white};
-    box-shadow: ${({ theme }) =>
-      `inset 0 0 0 1px ${theme.palette.common.white}${alpha32}`};
+    color: ${white};
+    box-shadow: inset 0 0 0 1px ${white}${alpha32};
 
     :hover {
-      box-shadow: ${({ theme }) =>
-        `inset 0 0 0 1px ${theme.palette.common.white}${alpha64}`};
+      box-shadow: inset 0 0 0 1px ${white}${alpha64};
     }
   }
 `;
