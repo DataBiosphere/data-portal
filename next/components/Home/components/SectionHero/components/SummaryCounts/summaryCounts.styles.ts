738d53af635e24210073ef27578df3439e1e9c44
Fran McDade <frano-m@users.noreply.github.com>
2023-10-18 16:17:37 +1000

    feat: update home page hero UI (#2205) (#2206)
    Co-authored-by: Fran McDade <franmcdade@Frans-MacBook-Pro.local>

--- next/components/Home/components/SectionHero/components/SummaryCounts/summaryCounts.styles.ts
+++ next/components/Home/components/SectionHero/components/SummaryCounts/summaryCounts.styles.ts
@@ -1,3 +1,5 @@
+import { mediaTabletUp } from "@clevercanary/data-explorer-ui/lib/styles/common/mixins/breakpoints";
+import { white } from "@clevercanary/data-explorer-ui/lib/styles/common/mixins/colors";
 import {
   textBodyLarge400,
   textHeadingLarge,
@@ -6,27 +8,42 @@ import styled from "@emotion/styled";
 
 export const Metrics = styled.div`
   box-sizing: content-box;
-  color: ${({ theme }) => theme.palette.common.white};
+  color: ${white};
   display: flex;
+  flex-wrap: wrap;
+  gap: 54px 64px;
   justify-content: space-between;
-  margin: 32px auto;
+  margin: 44px auto 64px;
   max-width: 1024px;
   padding: 0 16px;
+
+  ${mediaTabletUp} {
+    gap: unset;
+    margin: 40px auto;
+  }
 `;
 
 export const Metric = styled.div`
   display: grid;
   gap: 0 24px;
+  flex-basis: calc(50% - 32px); // minus gutter.
+  justify-content: flex-start;
 
   .MuiDivider-root {
     grid-row: 1 / 3; // Spans count and label.
     opacity: 0.5;
   }
+
+  ${mediaTabletUp} {
+    flex-basis: unset;
+  }
 `;
 
 export const Count = styled.div`
-  ${textHeadingLarge}
+  ${textHeadingLarge};
+  font-size: 32px;
   grid-column: 2;
+  line-height: 40px;
 `;
 
 export const Label = styled.div`
