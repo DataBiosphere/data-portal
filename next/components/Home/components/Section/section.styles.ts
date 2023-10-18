6fdeb1f24b9f159c2f5ef9bab08ff0aecfc78040
Fran McDade <frano-m@users.noreply.github.com>
2023-10-19 00:06:57 +1000

    feat: create home page section "what is the hca data portal?" (#2210) (#2211)

--- next/components/Home/components/Section/section.styles.ts
+++ next/components/Home/components/Section/section.styles.ts
@@ -1,13 +1,33 @@
-import { textHeadingLarge } from "@clevercanary/data-explorer-ui/lib/styles/common/mixins/fonts";
-import styled from "@emotion/styled/dist/emotion-styled.cjs";
+import { inkLight } from "@clevercanary/data-explorer-ui/lib/styles/common/mixins/colors";
+import {
+  textHeadingLarge,
+  textUppercase500,
+} from "@clevercanary/data-explorer-ui/lib/styles/common/mixins/fonts";
+import styled from "@emotion/styled";
+import { Divider } from "@mui/material";
 
 export const Section = styled.section`
   width: 100%;
 `;
 
-export const SectionHead = styled.h2`
+export const SectionHead = styled.h3`
   ${textHeadingLarge};
+  font-size: 32px;
+  line-height: 40px;
   margin: 0 auto;
   max-width: 816px;
   text-align: center;
+`;
+
+export const SectionOverline = styled.h4`
+  ${textUppercase500};
+  color: ${inkLight};
+  text-align: center;
+  margin: 0 0 8px;
+`;
+
+export const SectionDivider = styled(Divider)`
+  margin: 0 auto;
+  max-width: 1244px;
+  width: calc(100% - 32px); // minus gutter.
 `;
---
+++ next/components/Home/components/SectionWhatIsTheHCAPortal/sectionWhatIsTheHCAPortal.tsx
@@ -0,0 +1,71 @@
+import { StaticImage } from "@clevercanary/data-explorer-ui/lib/components/common/StaticImage/staticImage";
+import { PORTAL_URL } from "../../../../site-config/data-portal/dev/config";
+import {
+  Section,
+  SectionHead,
+  SectionOverline,
+} from "../Section/section.styles";
+import {
+  SectionContent,
+  Tile,
+  TileLink,
+  Tiles,
+  TileText,
+} from "./sectionWhatIsTheHCAPortal.styles";
+
+export const SectionWhatIsTheHCAPortal = (): JSX.Element => {
+  // Opens a new window with the given URL.
+  const onTileClick = (url: string): void => {
+    window.open(url, "_self");
+  };
+  return (
+    <Section>
+      <SectionContent>
+        <SectionOverline>What is the HCA Data Portal?</SectionOverline>
+        <SectionHead>
+          The HCA Data Portal stores and provides single-cell data contributed
+          by labs around the world. Anyone can contribute data, find data, or
+          access community tools and applications.
+        </SectionHead>
+        <Tiles>
+          <Tile onClick={(): void => onTileClick(`${PORTAL_URL}/contribute`)}>
+            <StaticImage
+              alt="labs-contribute"
+              src={"/bio-networks/home/labs-contribute.svg"}
+            />
+            <TileText>Labs contribute single-cell data</TileText>
+            <TileLink>Learn About Contributing</TileLink>
+          </Tile>
+          <Tile onClick={(): void => onTileClick(`${PORTAL_URL}/pipelines`)}>
+            <StaticImage
+              alt="pipeline-processing"
+              src="/bio-networks/home/pipeline-processing.svg"
+            />
+            <TileText>
+              We process and quality-check the data with our pipelines
+            </TileText>
+            <TileLink>Learn About Pipelines</TileLink>
+          </Tile>
+          <Tile onClick={(): void => onTileClick(`${PORTAL_URL}/explore`)}>
+            <StaticImage
+              alt="search-data"
+              src="/bio-networks/home/search-data.svg"
+            />
+            <TileText>
+              Anyone can find data to download or use for analysis
+            </TileText>
+            <TileLink>Start Searching</TileLink>
+          </Tile>
+          <Tile onClick={(): void => onTileClick(`${PORTAL_URL}/analyze`)}>
+            <StaticImage
+              alt="search-community"
+              src="/bio-networks/home/search-community.svg"
+            />
+            <TileText>Find community analysis tools and applications</TileText>
+            <TileLink>Explore Applications</TileLink>
+          </Tile>
+        </Tiles>
+      </SectionContent>
+    </Section>
+  );
+};
