ac8e584f71be0197e51e8d0d63bddfe0035c2e2c
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2023-10-18 16:45:41 +1000

    feat: update home page footer (#2207)

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -1,4 +1,5 @@
 import { Main } from "../components/Home/components/Layout/components/Main/main.styles";
+import { Footer } from "../components/Layout/components/Footer/footer.styles";
 import { HomeView } from "../views/HomeView/homeView";
 import { NextPageWithMain } from "./_app";
 
@@ -6,6 +7,7 @@ const Home: NextPageWithMain = () => {
   return <HomeView />;
 };
 
+Home.Footer = Footer;
 Home.Main = Main;
 
 export default Home;
