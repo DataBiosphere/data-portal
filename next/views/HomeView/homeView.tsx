2443e64b1b7d97c8455d4e8f69b462c53b2d40ac
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2023-10-18 22:02:51 +1000

    feat: create home page section "what is the hca data portal?" (#2210)

--- next/views/HomeView/homeView.tsx
+++ next/views/HomeView/homeView.tsx
@@ -1,12 +1,16 @@
+import { SectionDivider } from "../../components/Home/components/Section/section.styles";
 import { SectionBioNetworkAtlases } from "../../components/Home/components/SectionBioNetworkAtlases/sectionBioNetworkAtlases";
 import { SectionHero } from "../../components/Home/components/SectionHero/sectionHero";
 import { SectionStayUpToDate } from "../../components/Home/components/SectionStayUpToDate/sectionStayUpToDate";
+import { SectionWhatIsTheHCAPortal } from "../../components/Home/components/SectionWhatIsTheHCAPortal/sectionWhatIsTheHCAPortal";
 
 export const HomeView = (): JSX.Element => {
   return (
     <>
       <SectionHero />
       <SectionBioNetworkAtlases />
+      <SectionDivider />
+      <SectionWhatIsTheHCAPortal />
       <SectionStayUpToDate />
     </>
   );
