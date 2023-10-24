b70d2f5ba135c3f519350e29ac55b097a61dabe7
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2023-10-24 15:15:59 +1000

    feat: update theme components (#2219)

--- next/theme/common/components.ts
+++ next/theme/common/components.ts
@@ -1,7 +1,7 @@
 import { alpha08 } from "@clevercanary/data-explorer-ui/lib/theme/common/palette";
 import {
   TEXT_BODY_400_2_LINES,
-  TEXT_BODY_LARGE_500,
+  TEXT_BODY_500,
   TEXT_BODY_SMALL_500,
 } from "@clevercanary/data-explorer-ui/lib/theme/common/typography";
 import { Components, Theme } from "@mui/material";
@@ -55,12 +55,12 @@ export const MuiCssBaseline = (theme: Theme): Components["MuiCssBaseline"] => {
         fontSize: 14,
       },
       h3: {
-        ...theme.typography[TEXT_BODY_LARGE_500],
+        ...theme.typography[TEXT_BODY_500],
         margin: "0 0 8px",
       },
       h4: {
         ...theme.typography[TEXT_BODY_SMALL_500],
-        margin: "12px 0 8px",
+        margin: "0 0 8px",
       },
       li: {
         margin: "4px 0",
