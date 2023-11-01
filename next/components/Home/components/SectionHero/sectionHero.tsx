1626adbfce4a4694d380c0feefb46f369160f0a3
Fran McDade <frano-m@users.noreply.github.com>
2023-11-01 17:50:11 +1000

    feat: open home page links to explorer in a new tab (#2245) (#2246)
    Co-authored-by: Fran McDade <franmcdade@Frans-MacBook-Pro.local>

--- next/components/Home/components/SectionHero/sectionHero.tsx
+++ next/components/Home/components/SectionHero/sectionHero.tsx
@@ -1,5 +1,6 @@
 import { ButtonSecondary } from "@clevercanary/data-explorer-ui/lib/components/common/Button/components/ButtonSecondary/buttonSecondary";
 import { ButtonSecondaryOutline } from "@clevercanary/data-explorer-ui/lib/components/common/Button/components/ButtonSecondaryOutline/buttonSecondaryOutline";
+import { ANCHOR_TARGET } from "@clevercanary/data-explorer-ui/lib/components/Links/common/entities";
 import { PORTAL_URL } from "../../../../site-config/data-portal/dev/config";
 import { SummaryCounts } from "./components/SummaryCounts/summaryCounts";
 import {
@@ -11,6 +12,9 @@ import {
 } from "./sectionHero.styles";
 
 export const SectionHero = (): JSX.Element => {
+  const onExplore = (): void => {
+    window.open(`${PORTAL_URL}/explore`, ANCHOR_TARGET.BLANK);
+  };
   return (
     <Hero>
       <Headline>
@@ -21,9 +25,7 @@ export const SectionHero = (): JSX.Element => {
           Community generated, multi-omic, open and managed access data
         </Subhead>
         <CTAs>
-          <ButtonSecondary href={`${PORTAL_URL}/explore`}>
-            Explore Data
-          </ButtonSecondary>
+          <ButtonSecondary onClick={onExplore}>Explore Data</ButtonSecondary>
           <ButtonSecondaryOutline href={`${PORTAL_URL}/contribute`}>
             Contribute Data
           </ButtonSecondaryOutline>
