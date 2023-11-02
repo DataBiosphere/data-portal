85b14b90ed3a48f70a0d67353ab4ce00a2ff317b
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2023-11-02 18:18:46 +1000

    feat: added prod config browser url (#2249)

--- next/site-config/data-portal/prod/config.ts
+++ next/site-config/data-portal/prod/config.ts
@@ -1,8 +1,57 @@
+import { ANCHOR_TARGET } from "@clevercanary/data-explorer-ui/lib/components/Links/common/entities";
 import { SiteConfig } from "@clevercanary/data-explorer-ui/lib/config/entities";
-import devConfig from "../dev/config";
+import devConfig, { PORTAL_URL } from "../dev/config";
+
+const BROWSER_URL = "https://explore.data.humancellatlas.prod.clevercanary.com";
 
 const config: SiteConfig = {
   ...devConfig,
+  browserURL: BROWSER_URL,
+  layout: {
+    ...devConfig.layout,
+    header: {
+      ...devConfig.layout.header,
+      navLinks: [
+        {
+          label: "Explore",
+          target: ANCHOR_TARGET.BLANK,
+          url: BROWSER_URL,
+        },
+        {
+          label: "BioNetworks",
+          url: "/bio-networks",
+        },
+        {
+          label: "Guides",
+          url: `${PORTAL_URL}/guides`,
+        },
+        {
+          label: "Metadata",
+          url: `${PORTAL_URL}/metadata`,
+        },
+        {
+          label: "Pipelines",
+          url: `${PORTAL_URL}/pipelines`,
+        },
+        {
+          label: "Analysis Tools",
+          url: `${PORTAL_URL}/analyze`,
+        },
+        {
+          label: "Contribute",
+          url: `${PORTAL_URL}/contribute`,
+        },
+        {
+          label: "APIs",
+          url: `${PORTAL_URL}/apis`,
+        },
+        {
+          label: "Updates",
+          url: `${PORTAL_URL}/dcp-updates`,
+        },
+      ],
+    },
+  },
 };
 
 // Update gtmAuth for the prod environment lookup.
