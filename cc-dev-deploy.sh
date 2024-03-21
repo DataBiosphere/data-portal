d176cdb7aeceaa4e13aedbc2effdec1cc934b91d
Dave Rogers <dave@clevercanary.com>
2024-03-21 09:36:15 -0700

    fix: update cc dev gatsby deploy (#2381)

--- cc-dev-deploy.sh
+++ cc-dev-deploy.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
+rm -rf ./_metadata-schema
+rm -rf ./humancellatlas.github.io
+
+git clone https://github.com/HumanCellAtlas/metadata-schema.git _metadata-schema
+git clone https://github.com/HumanCellAtlas/humancellatlas.github.io
+
+n 14.16.0
+npm ci
+
 gatsby clean
 
-export GATSBY_EXPLORE_URL=https://hca-data-portal.dev.clevercanary.com/explore/
-export GATSBY_PORTAL_URL=https://hca-data-portal.dev.clevercanary.com/
+export GATSBY_EXPLORE_URL=https://explore.data.humancellatlas.dev.clevercanary.com/
+export GATSBY_PORTAL_URL=https://data.humancellatlas.dev.clevercanary.com/
 export GATSBY_FILE_SUMMARY_API_URL=https://service.dev.singlecell.gi.ucsc.edu/index/summary
 export GATSBY_TERM_FACETS_API_URL=https://service.dev.singlecell.gi.ucsc.edu/index/projects
 export GATSBY_SYSTEM_HEALTH_CHECK_API_URL=https://service.dev.singlecell.gi.ucsc.edu/health/progress
@@ -16,7 +25,7 @@ export GATSBY_ATLAS=hca
 gatsby build
 
 
-export BUCKET=s3://hca-data-portal.dev.clevercanary.com/
+export BUCKET=s3://wb8-portal/
 export SRCDIR=public/
 aws s3 sync  $SRCDIR $BUCKET --delete --profile excira
-aws cloudfront create-invalidation --distribution-id E2TY6LP6E2DGZ3 --paths "/*" --profile excira
+aws cloudfront create-invalidation --distribution-id E2SLGSHX4H0LO7 --paths "/*" --profile excira
