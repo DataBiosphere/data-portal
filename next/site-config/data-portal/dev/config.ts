99f793a769cf388d030064bf650717007945d001
David Rogers <dave@clevercanary.com>
2024-04-24 01:35:13 -0700

    Noopdog/2388 dcp37 (#2389)
    * chore: bump default catalog to dcp37 (#2388)
    
    * chore: bump gatsby  default catalog to dcp37 (#2388)

--- next/site-config/data-portal/dev/config.ts
+++ next/site-config/data-portal/dev/config.ts
@@ -25,7 +25,7 @@ const config: SiteConfig = {
       size: "100",
     },
     defaultParams: {
-      catalog: "dcp36",
+      catalog: "dcp37",
     },
     url: "https://service.azul.data.humancellatlas.org/index",
   },
