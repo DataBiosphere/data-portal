7e6a0b186d9af44d779f657f7d8820a6a9985985
EM <esthermcdade@EMs-MacBook-Pro.local>
2024-06-29 11:17:15 +1000

    feat: dp apis (#2411)

--- next/site-config/data-portal/dev/navigation/navigation.ts
+++ next/site-config/data-portal/dev/navigation/navigation.ts
@@ -1,7 +1,25 @@
-import { NavigationConfig } from "./entities";
+import { LAYOUT_STYLE_CONTRAST_LIGHTEST } from "@databiosphere/findable-ui/lib/components/Layout/components/ContentLayout/common/constants";
+import { ROUTES } from "../../../../routes/constants";
+import { NavigationConfig, NAVIGATION_KEY } from "./entities";
 import { GUIDES } from "./guides";
 
 // Site map for the MDX pages used to generate the navigation.
 export const navigation: NavigationConfig = {
+  apis: {
+    nodes: [
+      {
+        key: NAVIGATION_KEY.APIS,
+        layoutStyle: LAYOUT_STYLE_CONTRAST_LIGHTEST,
+        navigation: [
+          { label: "HCA Data Portal APIs", url: ROUTES.APIS },
+          {
+            label: "Data Browser API",
+            url: `${ROUTES.APIS}/api-documentation/data-browser-api`,
+          },
+        ],
+        slugs: [NAVIGATION_KEY.APIS, "data-browser-api"],
+      },
+    ],
+  },
   guides: GUIDES,
 };
