732913584bfda21bf4e4b22efd37ff0ecd7d8aa3
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2024-09-26 16:11:12 +1000

    feat: updated socials (#2447)

--- next/site-config/data-portal/dev/socialMedia.ts
+++ next/site-config/data-portal/dev/socialMedia.ts
@@ -1,21 +1,26 @@
 import { SocialMedia } from "@databiosphere/findable-ui/lib/components/Layout/components/Header/common/entities";
+import { ANCHOR_TARGET } from "@databiosphere/findable-ui/lib/components/Links/common/entities";
 import * as C from "../../../components/index";
 
 export const SOCIALS = {
   FACEBOOK: {
     label: "Facebook",
+    target: ANCHOR_TARGET.BLANK,
     url: "https://www.facebook.com/humancellatlas",
   },
   GITHUB: {
     label: "GitHub",
+    target: ANCHOR_TARGET.BLANK,
     url: "https://github.com/HumanCellAtlas",
   },
   LINKEDIN: {
     label: "LinkedIn",
+    target: ANCHOR_TARGET.BLANK,
     url: "https://www.linkedin.com/company/human-cell-atlas",
   },
   X: {
     label: "X",
+    target: ANCHOR_TARGET.BLANK,
     url: "https://twitter.com/humancellatlas",
   },
 };
