ba2739d063336246490d62f9a4764111d4b1f559
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2024-09-28 16:32:46 +1000

    feat: update pages paragraph font styles to a larger font (#2464)

--- next/theme/theme.ts
+++ next/theme/theme.ts
@@ -1,17 +1,38 @@
-import { createTheme, Theme } from "@mui/material";
+import { createAppTheme } from "@databiosphere/findable-ui/lib/theme/theme";
+import { createTheme, Theme, ThemeOptions } from "@mui/material";
+import { deepmerge } from "@mui/utils";
 import * as C from "./common/components";
 
 /**
- * Returns theme with customization.
- * @param theme -- Base theme
+ * Returns Data Portal customized theme.
+ * @param baseThemeOptions - Base theme options.
+ * @param themeOptions - Custom theme option overrides.
  * @returns theme with custom theme overrides.
  */
-export function mergeAppTheme(theme: Theme): Theme {
-  return createTheme(theme, {
+export function mergeAppTheme(
+  baseThemeOptions?: ThemeOptions,
+  themeOptions?: ThemeOptions
+): Theme {
+  // Merge custom options (palette, shadows, typography).
+  const customOptions = deepmerge(baseThemeOptions, { ...themeOptions });
+  // Create base app theme with custom options.
+  const baseAppTheme = createAppTheme(customOptions);
+  // Merge app components with base app theme.
+  const appTheme = createTheme(baseAppTheme, {
     components: {
-      MuiButton: C.MuiButton(theme),
-      MuiCssBaseline: C.MuiCssBaseline(theme),
-      MuiIconButton: C.MuiIconButton(theme),
+      MuiButton: C.MuiButton(baseAppTheme),
+      MuiCssBaseline: C.MuiCssBaseline(baseAppTheme),
+      MuiIconButton: C.MuiIconButton(baseAppTheme),
     },
   });
+  if (themeOptions?.components) {
+    // Return app theme - with custom component overrides.
+    return createTheme(
+      deepmerge(appTheme, {
+        components: themeOptions.components,
+      })
+    );
+  }
+  // Return app theme.
+  return appTheme;
 }
