3d0a835c8bddfd24663fa9305bf80d47c0641ba7
Jonah Paten <jonah@clevercanary.com>
2025-02-10 12:43:55 -0800

    feat: added monthly traffic summaries to analytics (#2707) (#2718)
    * feat: added monthly traffic summaries to analytics (#2707)
    
    * feat: added analytics explorer summary report (#2707)
    
    * chore: ran analytics for jan 2025 (#2707)

--- analytics/constants.py
+++ analytics/constants.py
@@ -1,20 +1,24 @@
 # CHANGE THESE VALUES TO GENERATE NEW REPORTS
 # The start and end dates of the current month (yyyy-mm-dd)
-START_DATE_CURRENT = "2024-12-01"
-END_DATE_CURRENT = "2024-12-31"
+START_DATE_CURRENT = "2025-01-01"
+END_DATE_CURRENT = "2025-01-31"
 # The start and end dates of the prior months
-START_DATE_PRIOR = "2024-11-01"
-END_DATE_PRIOR = "2024-11-30"
+START_DATE_PRIOR = "2024-12-01"
+END_DATE_PRIOR = "2024-12-31"
 # The name of the folder in which to save the report
-PARENT_FOLDER_NAME = "December 2024 (demos)"
+PARENT_FOLDER_NAME = "January 2025"
 
 # The name of the spreadsheet with the report
 SHEET_NAME = "HCA Portal"
 
 HCA_PORTAL_ID = "361323030"
-# Filter to exclude the Data Explorer
-HCA_BROWSER_EXCLUDE_FILTER = {"filter": {"fieldName": "hostName", "stringFilter": {"matchType": "EXACT", "value": "data.humancellatlas.org"}}}
+# Filter to include only the HCA Portal
+HCA_PORTAL_ONLY_FILTER = {"filter": {"fieldName": "hostName", "stringFilter": {"matchType": "EXACT", "value": "data.humancellatlas.org"}}}
+# Filter to include only the HCA Explorer
+HCA_EXPLORER_ONLY_FILTER = {"filter": {"fieldName": "hostName", "stringFilter": {"matchType": "EXACT", "value": "explore.data.humancellatlas.org"}}}
 SECRET_NAME = "HCA_ANALYTICS_REPORTING_CLIENT_SECRET_PATH"
-ANALYTICS_START = "2021-01-01"
+# The start date after which GA4 data is reliable
+ANALYTICS_START = "2023-07-01"
+HISTORIC_UA_DATA_PATH = "./users_over_time_history.json"
 
 OAUTH_PORT = 8082
