38a900617fc88bc18802a8658f11a5cb1527991b
hunterckx <118154470+hunterckx@users.noreply.github.com>
2025-03-18 17:47:06 -0700

    chore: update analytics report to adjust sheet order and activity over time start (#2730)

--- analytics/constants.py
+++ analytics/constants.py
@@ -1,12 +1,8 @@
 # CHANGE THESE VALUES TO GENERATE NEW REPORTS
-# The start and end dates of the current month (yyyy-mm-dd)
-START_DATE_CURRENT = "2025-01-01"
-END_DATE_CURRENT = "2025-01-31"
-# The start and end dates of the prior months
-START_DATE_PRIOR = "2024-12-01"
-END_DATE_PRIOR = "2024-12-31"
+# The date of the current month to report on (yyyy-mm)
+CURRENT_MONTH = "2025-02"
 # The name of the folder in which to save the report
-PARENT_FOLDER_NAME = "January 2025"
+PARENT_FOLDER_NAME = "February 2025"
 
 # The name of the spreadsheet with the report
 SHEET_NAME = "HCA Portal"
@@ -17,8 +13,12 @@ HCA_PORTAL_ONLY_FILTER = {"filter": {"fieldName": "hostName", "stringFilter": {"
 # Filter to include only the HCA Explorer
 HCA_EXPLORER_ONLY_FILTER = {"filter": {"fieldName": "hostName", "stringFilter": {"matchType": "EXACT", "value": "explore.data.humancellatlas.org"}}}
 SECRET_NAME = "HCA_ANALYTICS_REPORTING_CLIENT_SECRET_PATH"
-# The start date after which GA4 data is reliable
-ANALYTICS_START = "2023-07-01"
+# The start date after which GA4 data is reliable for combined portal + explorer
+GA4_START = "2023-07-01"
+# The start date after which GA4 data is reliable for the portal and explorer separately
+# This start date is shared by both portal and explorer because the report distinguishes them by hostname,
+# which is not something that differed between the two prior to this date
+GA4_START_SEPARATE = "2024-02-01"
 HISTORIC_UA_DATA_PATH = "./users_over_time_history.json"
 
 OAUTH_PORT = 8082
