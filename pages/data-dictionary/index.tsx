7e18b7ad99c454b22e842f0b64ac183309ab5503
MillenniumFalconMechanic <mim@clevercanary.com>
2025-05-12 16:27:01 -0700

    feat: added tier 1 data dictionary #2743

---
+++ pages/data-dictionary/index.tsx
@@ -0,0 +1,7 @@
+import { DataDictionaryView } from "@databiosphere/findable-ui/lib/views/DataDictionaryView/dataDictionaryView";
+
+const Page = (): JSX.Element => {
+  return <DataDictionaryView />;
+};
+
+export default Page;
