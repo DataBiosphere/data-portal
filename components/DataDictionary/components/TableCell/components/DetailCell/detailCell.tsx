eee45fb3db221d4c644b84decbfb6dd854d07b3a
Fran McDade <frano-m@users.noreply.github.com>
2025-06-18 08:43:03 -0700

    feat: make data dictionary more scannable (#2762) (#2776)

--- components/DataDictionary/components/TableCell/components/DetailCell/detailCell.tsx
+++ components/DataDictionary/components/TableCell/components/DetailCell/detailCell.tsx
@@ -2,85 +2,112 @@ import { CellContext } from "@tanstack/react-table";
 import { Attribute } from "../../../../../../viewModelBuilders/dataDictionaryMapper/types";
 import { Grid, Typography } from "@mui/material";
 import { TEXT_BODY_500 } from "@databiosphere/findable-ui/lib/theme/common/typography";
-import { StyledPaper, StyledGrid, StyledStack } from "./detailCell.styles";
+import {
+  StyledPaper,
+  StyledCell,
+  StyledStack,
+  StyledCollapse,
+  StyledButton,
+} from "./detailCell.styles";
 import { Link } from "@databiosphere/findable-ui/lib/components/Links/components/Link/link";
 import { TYPOGRAPHY_PROPS } from "@databiosphere/findable-ui/lib/styles/common/mui/typography";
 import { MarkdownRenderer } from "@databiosphere/findable-ui/lib/components/MarkdownRenderer/markdownRenderer";
 import { buildExample } from "./utils";
+import { useState } from "react";
+import { BUTTON_PROPS } from "@databiosphere/findable-ui/lib/styles/common/mui/button";
 
 export const DetailCell = ({
   row,
 }: CellContext<Attribute, unknown>): JSX.Element => {
+  const [isIn, setIsIn] = useState(false);
   return (
-    <StyledGrid container>
+    <StyledCell>
       <Grid>
         <Typography variant={TEXT_BODY_500}>Description</Typography>
         <Typography variant={TYPOGRAPHY_PROPS.VARIANT.TEXT_BODY_400_2_LINES}>
           <MarkdownRenderer value={row.original.description} />
         </Typography>
       </Grid>
-      {row.original.values && (
+      <StyledCollapse in={isIn}>
+        {row.original.values && (
+          <Grid>
+            <Typography variant={TEXT_BODY_500}>Allowed Values</Typography>
+            <Typography
+              variant={TYPOGRAPHY_PROPS.VARIANT.TEXT_BODY_400_2_LINES}
+            >
+              <MarkdownRenderer value={row.original.values} />
+            </Typography>
+          </Grid>
+        )}
+        {row.original.example && (
+          <Grid>
+            <Typography variant={TEXT_BODY_500}>Example</Typography>
+            <StyledStack direction="row">
+              {buildExample(row.original).map((example, i) => (
+                <StyledPaper key={i} elevation={0}>
+                  {example}
+                </StyledPaper>
+              ))}
+            </StyledStack>
+          </Grid>
+        )}
+        {row.original.rationale && (
+          <Grid>
+            <Typography variant={TEXT_BODY_500}>Rationale</Typography>
+            <Typography
+              variant={TYPOGRAPHY_PROPS.VARIANT.TEXT_BODY_400_2_LINES}
+            >
+              {row.original.rationale}
+            </Typography>
+          </Grid>
+        )}
         <Grid>
-          <Typography variant={TEXT_BODY_500}>Allowed Values</Typography>
-          <Typography variant={TYPOGRAPHY_PROPS.VARIANT.TEXT_BODY_400_2_LINES}>
-            <MarkdownRenderer value={row.original.values} />
-          </Typography>
+          <Typography variant={TEXT_BODY_500}>Source</Typography>
+          <Link {...row.original.source} />
         </Grid>
-      )}
-      {row.original.example && (
+        {row.original.annotations?.tier && (
+          <Grid>
+            <Typography variant={TEXT_BODY_500}>Tier</Typography>
+            <Typography
+              variant={TYPOGRAPHY_PROPS.VARIANT.TEXT_BODY_400_2_LINES}
+            >
+              <MarkdownRenderer
+                value={row.original.annotations.tier as string}
+              />
+            </Typography>
+          </Grid>
+        )}
+        {row.original.annotations?.bioNetworks && (
+          <Grid>
+            <Typography variant={TEXT_BODY_500}>BioNetworks</Typography>
+            <Typography
+              variant={TYPOGRAPHY_PROPS.VARIANT.TEXT_BODY_400_2_LINES}
+            >
+              <MarkdownRenderer
+                value={(row.original.annotations.bioNetworks as string[]).join(
+                  ", "
+                )}
+              />
+            </Typography>
+          </Grid>
+        )}
         <Grid>
-          <Typography variant={TEXT_BODY_500}>Example</Typography>
-          <StyledStack direction="row">
-            {buildExample(row.original).map((example, i) => (
-              <StyledPaper key={i} elevation={0}>
-                {example}
-              </StyledPaper>
-            ))}
-          </StyledStack>
-        </Grid>
-      )}
-      {row.original.rationale && (
-        <Grid>
-          <Typography variant={TEXT_BODY_500}>Rationale</Typography>
-          <Typography variant={TYPOGRAPHY_PROPS.VARIANT.TEXT_BODY_400_2_LINES}>
-            {row.original.rationale}
-          </Typography>
-        </Grid>
-      )}
-      <Grid>
-        <Typography variant={TEXT_BODY_500}>Source</Typography>
-        <Link {...row.original.source} />
-      </Grid>
-      {row.original.annotations?.tier && (
-        <Grid>
-          <Typography variant={TEXT_BODY_500}>Tier</Typography>
-          <Typography variant={TYPOGRAPHY_PROPS.VARIANT.TEXT_BODY_400_2_LINES}>
-            <MarkdownRenderer value={row.original.annotations.tier as string} />
-          </Typography>
-        </Grid>
-      )}
-      {row.original.annotations?.bioNetworks && (
-        <Grid>
-          <Typography variant={TEXT_BODY_500}>BioNetworks</Typography>
+          <Typography variant={TEXT_BODY_500}>AnnData Location</Typography>
           <Typography variant={TYPOGRAPHY_PROPS.VARIANT.TEXT_BODY_400_2_LINES}>
             <MarkdownRenderer
-              value={(row.original.annotations.bioNetworks as string[]).join(
-                ", "
-              )}
+              value={
+                (row.original.annotations?.annDataLocation as string) || "None"
+              }
             />
           </Typography>
         </Grid>
-      )}
-      <Grid>
-        <Typography variant={TEXT_BODY_500}>AnnData Location</Typography>
-        <Typography variant={TYPOGRAPHY_PROPS.VARIANT.TEXT_BODY_400_2_LINES}>
-          <MarkdownRenderer
-            value={
-              (row.original.annotations?.annDataLocation as string) || "None"
-            }
-          />
-        </Typography>
-      </Grid>
-    </StyledGrid>
+      </StyledCollapse>
+      <StyledButton
+        onClick={() => setIsIn((i) => !i)}
+        variant={BUTTON_PROPS.VARIANT.TEXT}
+      >
+        {isIn ? "Show less" : "Show more"}
+      </StyledButton>
+    </StyledCell>
   );
 };
