e68fbc69853389d77ca7b356fd270876fa1044ed
Fran McDade <frano-m@users.noreply.github.com>
2025-06-18 15:43:42 -0700

    feat: data dictionary provider and global search (#2743) (#2775)
    * feat: data dictionary provider and global search (#2743)
    
    * fix: data dictionary page props type (#2743)
    
    * fix: global search inside markdown cells (#2743)
    
    ---------
    
    Co-authored-by: Fran McDade <franmcdade@Frans-MacBook-Pro.local>

--- viewModelBuilders/dataDictionaryMapper/tableOptions.ts
+++ viewModelBuilders/dataDictionaryMapper/tableOptions.ts
@@ -1,16 +1,24 @@
 import { TableOptions } from "@tanstack/react-table";
 import { Attribute } from "./types";
+import { COLUMN_DEFS } from "./columnDefs";
 
 export const TABLE_OPTIONS: Omit<
   TableOptions<Attribute>,
-  "columns" | "data" | "getCoreRowModel"
+  "data" | "getCoreRowModel"
 > = {
+  columns: COLUMN_DEFS,
   initialState: {
     columnVisibility: {
+      annDataLocation: false,
       bioNetwork: false,
       classKey: true,
+      description: false,
+      name: false,
+      rationale: false,
       required: false,
       tier: false,
+      title: false,
+      values: false,
     },
     expanded: true,
     grouping: ["classKey"],
