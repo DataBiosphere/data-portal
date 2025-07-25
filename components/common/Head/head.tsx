f4e6559c6a969b9964691e076369dcc7a1ca1b5c
Fran McDade <frano-m@users.noreply.github.com>
2025-07-25 15:36:29 +1000

    content: check for and remove internal links to the following urls (#2774) (#2806)
    * content: check for and remove internal links to the following urls (#2774)
    
    * content: updated internal metadata links (#2774)
    
    * content: dcp update link to metadata schema (#2774)
    
    ---------

--- components/common/Head/head.tsx
+++ components/common/Head/head.tsx
@@ -21,12 +21,6 @@ export const Head = ({ appTitle, pageTitle }: HeadProps): JSX.Element => {
         type="image/png"
       />
       <link
-        href="/favicon-32x32.png"
-        rel="icon"
-        sizes="32x32"
-        type="image/png"
-      />
-      <link
         href="/hca-bio-networks/favicons/apple-touch-icon.png"
         rel="apple-touch-icon"
         sizes="180x180"
