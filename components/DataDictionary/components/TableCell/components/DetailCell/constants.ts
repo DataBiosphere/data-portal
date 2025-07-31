6b066c057c4ca59ba5d38d610137966c23aeb7ae
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2025-07-31 11:59:59 +1000

    fix: bionetworks display of 'All' by hard coding the total number of bionetworks (#2815)

--- components/DataDictionary/components/TableCell/components/DetailCell/constants.ts
+++ components/DataDictionary/components/TableCell/components/DetailCell/constants.ts
@@ -1,6 +1,8 @@
 import { TYPOGRAPHY_PROPS as MUI_TYPOGRAPHY_PROPS } from "@databiosphere/findable-ui/lib/styles/common/mui/typography";
 import { TypographyProps } from "@mui/material";
 
+export const BIO_NETWORK_COUNT = 18;
+
 export const TYPOGRAPHY_PROPS: TypographyProps = {
   component: "div",
   gutterBottom: true,
